aa4724657d2315268b16741e5809c6c261cccedf
andre <andre>
2002-01-15 10:27:40 +0000

    msg_sec.h file was missing

---
+++ source/security/sec/msg_sec.h
@@ -0,0 +1,47 @@
+#ifndef __MSG_SEC_H
+#define __MSG_SEC_H
+/*___INFO__MARK_BEGIN__*/
+/*************************************************************************
+ * 
+ *  The Contents of this file are made available subject to the terms of
+ *  the Sun Industry Standards Source License Version 1.2
+ * 
+ *  Sun Microsystems Inc., March, 2001
+ * 
+ * 
+ *  Sun Industry Standards Source License Version 1.2
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.2 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+ * 
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ * 
+ *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ * 
+ *   Copyright: 2001 by Sun Microsystems, Inc.
+ * 
+ *   All Rights Reserved.
+ * 
+ ************************************************************************/
+/*___INFO__MARK_END__*/
+
+
+#include "basis_types.h"
+
+
+#define MSG_SGETEXT_CAROOTNOTFOUND_S         _("CA_ROOT directory "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_CALOCALROOTNOTFOUND_S         _("CA_LOCAL_ROOT directory "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_CAKEYFILENOTFOUND_S      _("CA private key "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_CACERTFILENOTFOUND_S     _("CA certificate "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_KEYFILENOTFOUND_S     _("key "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_CERTFILENOTFOUND_S     _("certificate "SFQ" doesn't exist\n")
+#define MSG_SGETEXT_USERNOTFOUND_S           _("user "SFQ" not found in password database\n")
+
+#endif /* __MSG_SEC_H */

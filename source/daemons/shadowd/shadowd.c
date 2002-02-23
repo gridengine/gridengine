220edff021da80d182b5ead70e33b9a596dbd7b7
svdavidson <svdavidson>
2002-02-23 02:06:13 +0000

    Support environment variables to override default timeout values for shadow daemon taking over qmaster function.

--- source/daemons/shadowd/shadowd.c
+++ source/daemons/shadowd/shadowd.c
@@ -91,6 +91,10 @@
 #  define DELAY_TIME          600 
 #endif
 
+static int check_interval = CHECK_INTERVAL;
+static int get_active_interval = GET_ACTIVE_INTERVAL;
+static int delay_time = DELAY_TIME;
+
 char binpath[SGE_PATH_MAX];
 char oldqmaster[SGE_PATH_MAX];
 
@@ -187,6 +191,21 @@ char **argv
 
    DENTER_MAIN(TOP_LAYER, "sge_shadowd");
    
+   /* initialize recovery control variables */
+   {
+      char *s;
+      int val;
+      if ((s=getenv("SGE_CHECK_INTERVAL")) &&
+          sscanf(s, "%d", &val) == 1)
+         check_interval = val;
+      if ((s=getenv("SGE_GET_ACTIVE_INTERVAL")) &&
+          sscanf(s, "%d", &val) == 1)
+         get_active_interval = val;
+      if ((s=getenv("SGE_DELAY_TIME")) &&
+          sscanf(s, "%d", &val) == 1)
+         delay_time = val;
+   }
+         
    /* This needs a better solution */
    umask(022);
 
@@ -287,7 +306,7 @@ char **argv
 
    delay = 0;
    while (TRUE) {
-      sleep(CHECK_INTERVAL);
+      sleep(check_interval);
 
       if (shut_me_down) {
          extern u_long32 logginglevel;
@@ -310,12 +329,12 @@ char **argv
       /* We could read two times the heartbeat */
       if (last_heartbeat != -1 && 
           heartbeat != -1 && 
-          (now - last >= (GET_ACTIVE_INTERVAL + delay))) {
+          (now - last >= (get_active_interval + delay))) {
          delay = 0;
          if ((last_heartbeat - heartbeat) == 0) {
             DPRINTF(("heartbeat not changed since seconds: %d\n", 
                (int) (now - last)));
-            delay = DELAY_TIME;
+            delay = delay_time;
             if (!(ret = check_if_valid_shadow(path.shadow_masters_file))) {
                if (qmaster_lock(QMASTER_LOCK_FILE)) {
                   ERROR((SGE_EVENT, MSG_SHADOWD_FAILEDTOLOCKQMASTERSOMBODYWASFASTER));

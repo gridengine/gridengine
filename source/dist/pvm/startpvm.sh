ecf0d0453faddd415e23a2feefb97d51f2d62a41
andreas <andreas>
2002-03-04 14:16:16 +0000

    AH-2002-03-04-2: Enhancem.: Added PVM 3.4.4 Virtual Machine ID setting                             to startpvm.sh from Andrew Shewmaker Issue number: Obtained from: Submitted by: Reviewed by:

--- source/dist/pvm/startpvm.sh
+++ source/dist/pvm/startpvm.sh
@@ -119,6 +119,10 @@ fi
 
 cut -f1 -d" " $pe_hostfile >> $pvm_hostfile
 
+# PVM 3.4.4 knows a Virtual Machine IDs for supporting 
+# overlapping PVM instances of the same user
+# PVM_VMID=$JOB_ID; export PVM_VMID
+
 # startup and wait for daemons
 $SGE_ROOT/pvm/bin/$ARC/start_pvm -h $NHOSTS $PVM_ROOT/lib/pvmd $pvm_hostfile
 

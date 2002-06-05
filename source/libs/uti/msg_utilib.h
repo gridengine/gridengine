640c77c7abc1bbb9b774d0f46c2c94afc84cde36
Joachim Gabler <jgabler@univa.com>
2002-06-05 14:40:52 +0000

    JG-2002-06-05-3: Cleanup:   Replaced %s in messages with macros SFN and SFQ                             for utilib.                  Issue:     #252 Issue number: Obtained from: Submitted by: Reviewed by:

--- source/libs/uti/msg_utilib.h
+++ source/libs/uti/msg_utilib.h
@@ -47,12 +47,12 @@
 /* 
 ** utilib/sge_afsutil.c
 */ 
-#define MSG_TOKEN_NOSTART_S         _("can't start set_token_command \"%s\"")
-#define MSG_TOKEN_NOWRITEAFS_S      _("can't write AFS token to set_token_command \"%s\"")
-#define MSG_TOKEN_NOSETAFS_SI       _("failed to set AFS token - set_token_command \"%s\" returned with exit status %d")
+#define MSG_TOKEN_NOSTART_S         _("can't start set_token_command "SFQ)
+#define MSG_TOKEN_NOWRITEAFS_S      _("can't write AFS token to set_token_command "SFQ)
+#define MSG_TOKEN_NOSETAFS_SI       _("failed to set AFS token - set_token_command "SFQ" returned with exit status %d")
 #define MSG_COMMAND_NOPATHFORTOKEN  _("can't get path for command to get AFS token\n")
-#define MSG_COMMAND_NOFILESTATUS_S  _("can't determine file status of command \"%s\"\n")
-#define MSG_COMMAND_NOTEXECUTABLE_S _("command \"%s\" is not executable\n")
+#define MSG_COMMAND_NOFILESTATUS_S  _("can't determine file status of command "SFQ"\n")
+#define MSG_COMMAND_NOTEXECUTABLE_S _("command "SFQ" is not executable\n")
 
 /*
 ** utilib/sge_arch.c
@@ -72,31 +72,31 @@
 /* 
 ** utilib/sge_bitop.c
 */ 
-#define MSG_MEMORY_NOMEMORYFORBYTEARRAY_S    _("%s: can't malloc memory for byte array\n")
-#define MSG_FILE_NOOPENFORWRITEING_SS        _("%s: unable to open file %s for writing!\n")
+#define MSG_MEMORY_NOMEMORYFORBYTEARRAY_S    _(SFN": can't malloc memory for byte array\n")
+#define MSG_FILE_NOOPENFORWRITEING_SS        _(SFN": unable to open file "SFN" for writing!\n")
 
 
 /* 
 ** utilib/sge_unistd.c
 */ 
-#define MSG_FILE_NOCDTODIRECTORY_S           _("can't change to directory \"%s\"")
+#define MSG_FILE_NOCDTODIRECTORY_S           _("can't change to directory "SFQ)
 
 
 /* 
 ** utilib/sge_daemonize.c
 */ 
-#define MSG_PROC_FIRSTFORKFAILED_S           _("1st fork() failed while daemonizing: %s\n")
-#define MSG_PROC_SECONDFORKFAILED_S          _("2nd fork() failed while daemonizing: %s\n")
+#define MSG_PROC_FIRSTFORKFAILED_S           _("1st fork() failed while daemonizing: "SFN"\n")
+#define MSG_PROC_SECONDFORKFAILED_S          _("2nd fork() failed while daemonizing: "SFN"\n")
 
 /* 
 ** utilib/sge_dir.c
 */ 
 #define MSG_POINTER_NULLPARAMETER            _("NULL parameter")
-#define MSG_FILE_OPENDIRFAILED_SS            _("opendir(%s) failed: %s\n")
-#define MSG_FILE_STATFAILED_SS               _("stat(%s) failed: %s\n")
+#define MSG_FILE_OPENDIRFAILED_SS            _("opendir("SFN") failed: "SFN"\n")
+#define MSG_FILE_STATFAILED_SS               _("stat("SFN") failed: "SFN"\n")
 #define MSG_FILE_RECURSIVERMDIRFAILED        _("==================== recursive_rmdir() failed\n")
-#define MSG_FILE_UNLINKFAILED_SS             _("unlink(%s) failed: %s\n")
-#define MSG_FILE_RMDIRFAILED_SS              _("rmdir(%s) failed: %s\n")
+#define MSG_FILE_UNLINKFAILED_SS             _("unlink("SFN") failed: "SFN"\n")
+#define MSG_FILE_RMDIRFAILED_SS              _("rmdir("SFN") failed: "SFN"\n")
 
 
 /* 
@@ -113,7 +113,7 @@
 ** utilib/sge_loadmem.c
 */ 
 #define MSG_SYSTEM_NOPAGESIZEASSUME8192      _("can't determine system page size - assuming 8192")
-#define MSG_SYSTEM_TABINFO_FAILED_SS         _("tabinfo(\"%s\", ...) failed, %s\n")            
+#define MSG_SYSTEM_TABINFO_FAILED_SS         _("tabinfo("SFQ", ...) failed, "SFN"\n")            
 #define MSG_MEMORY_MALLOCFAILED_D            _("malloc("U32CFormat") failed" )
 
 
@@ -122,7 +122,7 @@
 */ 
 #define MSG_LOG_CRITICALERROR                _("critical error: ")
 #define MSG_LOG_ERROR                        _("error: ")
-#define MSG_LOG_CALLEDLOGGINGSTRING_S        _("logging called with %s logging string")
+#define MSG_LOG_CALLEDLOGGINGSTRING_S        _("logging called with "SFN" logging string")
 #define MSG_LOG_ZEROLENGTH                   _("zero length")
 #define MSG_POINTER_NULL                     _("NULL")
 
@@ -131,40 +131,40 @@
 */ 
 #define MSG_SYSTEM_EXECBINSHFAILED              _("can't exec /bin/sh\n")
 #define MSG_SYSTEM_NOROOTRIGHTSTOSWITCHUSER     _("you have to be root to become another user\n" )
-#define MSG_SYSTEM_NOUSERFOUND_SS               _("can't get user %s: %s\n")
-#define MSG_SYSTEM_INITGROUPSFORUSERFAILED_ISS  _("res = %d, can't initialize groups for user %s: %s\n")
-#define MSG_SYSTEM_SWITCHTOUSERFAILED_SS        _("can't change to user %s: %s\n")
-#define MSG_SYSTEM_FAILOPENPIPES_SS             _("failed opening pipes for %s: %s\n")
+#define MSG_SYSTEM_NOUSERFOUND_SS               _("can't get user "SFN": "SFN"\n")
+#define MSG_SYSTEM_INITGROUPSFORUSERFAILED_ISS  _("res = %d, can't initialize groups for user "SFN": "SFN"\n")
+#define MSG_SYSTEM_SWITCHTOUSERFAILED_SS        _("can't change to user "SFN": "SFN"\n")
+#define MSG_SYSTEM_FAILOPENPIPES_SS             _("failed opening pipes for "SFN": "SFN"\n")
 
 
 /* 
 ** utilib/sge_processes_irix.c
 */ 
-#define MSG_FILE_OPENFAILED_SS                  _("failed opening %s: %s")
-#define MSG_SYSTEM_GETPIDSFAILED_S              _("getpidsOfJob: ioctl(%s, PIOCSTATUS) failed\n")
-#define MSG_PROC_KILL_IIS                       _("kill(%d, %d): %s")
+#define MSG_FILE_OPENFAILED_SS                  _("failed opening "SFN": "SFN"")
+#define MSG_SYSTEM_GETPIDSFAILED_S              _("getpidsOfJob: ioctl("SFN", PIOCSTATUS) failed\n")
+#define MSG_PROC_KILL_IIS                       _("kill(%d, %d): "SFN"")
 #define MSG_PROC_KILLISSUED_II                  _("kill(%d, %d) issued")
 
 
 /* 
 ** utilib/sge_put_get_file.c
 */ 
-#define MSG_FILE_WRITEOPENFAILED_SS             _("couldn't open %s for writing: %s\n")
-#define MSG_FILE_READOPENFAILED_SS              _("couldn't open %s for reading: %s")
+#define MSG_FILE_WRITEOPENFAILED_SS             _("couldn't open "SFN" for writing: "SFN"\n")
+#define MSG_FILE_READOPENFAILED_SS              _("couldn't open "SFN" for reading: "SFN"")
 
 
 /* 
 ** utilib/sge_signal.c
 */ 
 #define MSG_PROC_UNKNOWNSIGNAL                  _("unknown signal")
-#define MSG_PROC_SIGACTIONFAILED_IS             _("sigaction for signal %d failed: %s")
+#define MSG_PROC_SIGACTIONFAILED_IS             _("sigaction for signal %d failed: "SFN"")
 
 
 /* 
 ** utilib/sge_str_from_file.c
 */ 
-#define MSG_FILE_FOPENFAILED_SS                 _("fopen("SFQ") failed: %s\n")
-#define MSG_FILE_FREADFAILED_SS                 _("fread("SFQ") failed: %s\n")
+#define MSG_FILE_FOPENFAILED_SS                 _("fopen("SFQ") failed: "SFN"\n")
+#define MSG_FILE_FREADFAILED_SS                 _("fread("SFQ") failed: "SFN"\n")
 #define MSG_FILE_OPENFAILED_S                   _("cant open file "SFQ"\n")
 #define MSG_FILE_WRITEBYTESFAILED_IS            _("cant write %d bytes into file "SFQ"\n")
 
@@ -177,7 +177,7 @@
 ** utilib/sge_switch_user.c
 */ 
 #define MSG_POINTER_SETADMINUSERNAMEFAILED      _("set_admin_username() with zero length username")
-#define MSG_SYSTEM_ADMINUSERNOTEXIST_S          _("admin_user \"%s\" does not exist")
+#define MSG_SYSTEM_ADMINUSERNOTEXIST_S          _("admin_user "SFQ" does not exist")
 #define MSG_SYSTEM_CANTRUNASCALLINGUSER         _("cannot run as calling user")
 #define MSG_SWITCH_USER_NOT_INITIALIZED         _("Module 'sge_switch_user' not initialized")
 #define MSG_SWITCH_USER_NOT_ROOT                _("User 'root' did not start the application\n")
@@ -192,36 +192,16 @@
 **  former SGE_TEXT:
 */
 #define MSG_SGETEXT_SHOULD_BE_ROOT_S            _("should be root to start "SFN"\n")
-/* 
-#define MSG_SGETEXT_INVALIDHOSTINQUEUE_S        _("invalid hostname "SFQ" associated with queue\n")
-#define MSG_SGETEXT_MODIFIEDINLIST_SSUS         _(""SFN"@"SFN" modified \"" U32CFormat "\" in "SFN" list\n")
-#define MSG_SGETEXT_CANTRESOLVEUSER_S           _("unknown user name "SFQ"\n")   
-#define MSG_SGETEXT_CANTRESOLVEGROUP_S          _("unknown group name "SFQ"\n")  
-#define MSG_SGETEXT_NOCOMMD_SS                  _("unable to contact commd at host "SFN" using service "SFN"\n")
-#define MSG_SGETEXT_NOPERM                      _("no permissions for this operation\n")
-#define MSG_SGETEXT_CANTFINDACL_S               _("unable to find referenced access list "SFQ"\n")
-#define MSG_SGETEXT_SHOULD_BE_ROOT_S            _("should be root to start "SFN"\n")
-#define MSG_SGETEXT_STILL_REFERENCED_SS         _("remove reference to "SFQ" in subordinates of queue "SFQ" before deletion\n") 
-#define MSG_SGETEXT_NO_SECURITY_LEVEL_FOR_S           _("denied: missing security level for "SFN"\n")
-#define MSG_SGETEXT_MAY_NOT_CHG_QHOST_S               _("may not change host of queue "SFQ"\n")
-#define MSG_SGETEXT_UP_REFERENCED_TWICE_SS            _("denied: share tree contains reference to unknown "SFN" "SFQ"\n")   
-#define MSG_SGETEXT_NO_PROJECT                        _("job rejected: no project assigned to job\n")     
-#define MSG_SGETEXT_UNABLETORETRIEVE_I                _("unable to retrieve value for system limit (%d)\n")     
-
-#define MSG_SYSTEM_INVALIDERRORNUMBER                 _("invalid error number")
-#define MSG_SYSTEM_GOTNULLASERRORTEXT                 _("no error text available")
-*/
-
 
 /* 
 ** utilib/sge_uidgid.c
 */ 
-#define MSG_SYSTEM_GETPWUIDFAILED_US               _("getpwuid("pid_t_fmt") failed: %s\n")
-#define MSG_SYSTEM_GETGRGIDFAILED_US               _("getgrgid("pid_t_fmt") failed: %s\n")
+#define MSG_SYSTEM_GETPWUIDFAILED_US               _("getpwuid("pid_t_fmt") failed: "SFN"\n")
+#define MSG_SYSTEM_GETGRGIDFAILED_US               _("getgrgid("pid_t_fmt") failed: "SFN"\n")
 #define MSG_SYSTEM_GETPWNAMFAILED_S   _("can't get password entry for user "SFQ". Either the user does not exist or NIS error!") 
 #define MSG_SYSTEM_CHANGEUIDORGIDFAILED         _("tried to change uid/gid without being root")
-#define MSG_SYSTEM_GIDLESSTHANMINIMUM_SUI       _("gid of user %s ("gid_t_fmt") less than minimum allowed in conf (%d)")
-#define MSG_SYSTEM_UIDLESSTHANMINIMUM_SUI       _("uid of user %s ("uid_t_fmt") less than minimum allowed in conf (%d)")
+#define MSG_SYSTEM_GIDLESSTHANMINIMUM_SUI       _("gid of user "SFN" ("gid_t_fmt") less than minimum allowed in conf (%d)")
+#define MSG_SYSTEM_UIDLESSTHANMINIMUM_SUI       _("uid of user "SFN" ("uid_t_fmt") less than minimum allowed in conf (%d)")
 #define MSG_SYSTEM_SETGIDFAILED_U               _("setgid("gid_t_fmt") failed")
 #define MSG_SYSTEM_SETUIDFAILED_U               _("setuid("uid_t_fmt") failed")
 #define MSG_SYSTEM_SETEGIDFAILED_U              _("setegid("gid_t_fmt") failed")
@@ -236,14 +216,14 @@
 */ 
 #define MSG_MEMORY_MALLOCFAILED                    _("malloc() failure\n")
 #define MSG_MEMORY_REALLOCFAILED                   _("realloc() failure\n") 
-#define MSG_GDI_STRING_LENGTHEXCEEDED_SI           _("strlen($%s) exceeds MAX_STRING_SIZE %d\n") 
+#define MSG_GDI_STRING_LENGTHEXCEEDED_SI           _("strlen($"SFN") exceeds MAX_STRING_SIZE %d\n") 
 
 /* 
 ** utilib/sge_unistd.c
 */ 
 #define MSG_POINTER_SUFFIXISNULLINSGEUNLINK        _("suffix == NULL in sge_unlink()\n") 
 #define MSG_VAR_PATHISNULLINSGEMKDIR            _("path == NULL in sge_mkdir()")
-#define MSG_FILE_CREATEDIRFAILED_SS             _("can't create directory \"%s\": %s\n")
+#define MSG_FILE_CREATEDIRFAILED_SS             _("can't create directory "SFQ": "SFN"\n")
 
 /*
 ** utilib/sge_hostname.c

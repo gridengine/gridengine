698d451c9c21f251eaed2c9c89e3f269447a72b9
crei <crei>
2002-06-13 09:31:16 +0000

    CR-2002-06-13-2: Testsuite: merged qstat -l / -q /-pe / -U option tests                             from V53_beta2_BRANCH                  Issue:     #286
    Issue number: 286
    Obtained from:
    Submitted by:
    Reviewed by:

--- testsuite/tcl_files/sge_procedures.tcl
+++ testsuite/tcl_files/sge_procedures.tcl
@@ -162,6 +162,8 @@ proc resolve_version { { internal_number -100 } } {
    set versions(SGE_5.3beta2_1)      2
    set versions(SGEEE_5.3beta2_2)    2
    set versions(SGE_5.3beta2_2)      2
+   set versions(SGEEE_5.3.1beta1)    2
+   set versions(SGE_5.3.1beta1)      2
    set versions(SGEEE_pre6.0_(Maintrunk))    2
    set versions(SGE_pre6.0_(Maintrunk))      2
 

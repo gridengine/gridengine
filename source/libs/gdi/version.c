8139f73c9ed9fc5ce97327346f3ada98923e3f2d
andy <andy>
2002-07-08 12:44:37 +0000

    .

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "5.3p2beta1";
+const char GDI_VERSION[] = "5.3.1beta2";
 
 #ifdef ADD_COPYRIGHT
 #  include "copyright.h"

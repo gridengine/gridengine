beee796ed8771d2b89ffc898c4344b0fdb9d6c9b
andy <andy>
2002-07-18 08:00:12 +0000

    AS-2002-07-18-0 installation changes, version set to beta3

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "5.3.1beta2";
+const char GDI_VERSION[] = "5.3.1beta3";
 
 #ifdef ADD_COPYRIGHT
 #  include "copyright.h"

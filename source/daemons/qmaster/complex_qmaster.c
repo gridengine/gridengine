796ca4a0d732322f5babd8a6c67fc5d9a21a15fa
Ernst Bablick <ebablick@univa.com>
2002-07-23 09:43:27 +0000

    EB-2002-07-23-0: Bugfix:    qsub/qalter -l <shortcut_name>... might select                             wrong resource.                  Issue:     #328                  Bugtraq:   #4718880                  Changed:   sge_qmaster

--- source/daemons/qmaster/complex_qmaster.c
+++ source/daemons/qmaster/complex_qmaster.c
@@ -199,11 +199,13 @@ int sub_command
 
          lListElem *cr, *qep, *hep, *cxep;
          const char *name;
+         const char *shortcut;
 
          name = lGetString(cep, CE_name);
+         shortcut = lGetString(cep, CE_shortcut);
 
          if (!name ||
-             !lGetString(cep, CE_shortcut) ||
+             !shortcut ||
              !lGetString(cep, CE_stringval) ||
              !lGetString(cep, CE_default)) {
             ERROR((SGE_EVENT, MSG_CPLX_ATTRIBISNULL_SS,
@@ -239,9 +241,11 @@ int sub_command
             } 
 
             /* search for shortcut */
-            if (find_attribute_in_complex_list(name, lFirst(lGetList(cxep, CX_entries)))) {
-               ERROR((SGE_EVENT, MSG_CPLX_SHORTCUTALREADY_SS, name, lGetString(cxep, CX_name))); 
-               answer_list_add(alpp, SGE_EVENT, STATUS_ESEMANTIC, ANSWER_QUALITY_ERROR);
+            if (find_attribute_in_complex_list(shortcut, lFirst(lGetList(cxep, CX_entries)))) {
+               ERROR((SGE_EVENT, MSG_CPLX_SHORTCUTALREADY_SS, shortcut, 
+                      lGetString(cxep, CX_name))); 
+               answer_list_add(alpp, SGE_EVENT, STATUS_ESEMANTIC, 
+                               ANSWER_QUALITY_ERROR);
                goto ERROR;
             } 
          }
@@ -255,9 +259,11 @@ int sub_command
          } 
 
          /* search for shortcut in remaining attribs */
-         if (find_attribute_in_complex_list(name, lNext(cep))) {
-            ERROR((SGE_EVENT, MSG_CPLX_SHORTCUTALREADY_SS, name, complex_name)); 
-            answer_list_add(alpp, SGE_EVENT, STATUS_ESEMANTIC, ANSWER_QUALITY_ERROR);
+         if (find_attribute_in_complex_list(shortcut, lNext(cep))) {
+            ERROR((SGE_EVENT, MSG_CPLX_SHORTCUTALREADY_SS, shortcut, 
+                   complex_name)); 
+            answer_list_add(alpp, SGE_EVENT, STATUS_ESEMANTIC, 
+                            ANSWER_QUALITY_ERROR);
             goto ERROR;
          } 
       

43a737604509fca93c800c9c1bbe08455e625b74
andy <andy>
2002-07-25 13:06:01 +0000

    set version to beta4

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "5.3.1beta3";
+const char GDI_VERSION[] = "5.3.1beta4";
 
 #ifdef ADD_COPYRIGHT
 #  include "copyright.h"

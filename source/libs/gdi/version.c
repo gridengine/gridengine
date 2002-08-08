7161afb7ba623962e9c1812ab1a2b4850689fc30
andy <andy>
2002-08-08 14:45:28 +0000

    reverting this decision to avoid confusion wiht bugtraq versions

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "5.3p2.beta6";
+const char GDI_VERSION[] = "5.3.1beta6";
 
 #ifdef ADD_COPYRIGHT
 #  include "copyright.h"

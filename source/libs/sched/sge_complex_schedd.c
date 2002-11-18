6b4dc001df36a8a9a5229ff58a630b45b1482a51
andreas <andreas>
2002-11-18 16:07:03 +0000

    AH-2002-11-18-1  Bugfix:    Complex values for user defined complexes                               are rejected with global host.                  IZ         418                  Bugtraq    4776754                  Review     AS Issue number: Obtained from: Submitted by: Reviewed by:

--- source/libs/sched/sge_complex_schedd.c
+++ source/libs/sched/sge_complex_schedd.c
@@ -409,8 +409,8 @@ int recompute_debitation_dependent
    DENTER(TOP_LAYER, "global_complexes2scheduler");
 
    /* build global complex and add it to result */
-   if ((complex = lGetElemStr(complex_list, CX_name, "global")))
-      fillComplexFromHost(new_complex_list, complex_list, global_host, complex, DOMINANT_LAYER_GLOBAL, recompute_debitation_dependent);
+   complex = lGetElemStr(complex_list, CX_name, "global");
+   fillComplexFromHost(new_complex_list, complex_list, global_host, complex, DOMINANT_LAYER_GLOBAL, recompute_debitation_dependent);
 
    DEXIT;
    return 0;
@@ -443,10 +443,9 @@ int recompute_debitation_dependent
    }
 
    /* build host complex and add it to result */
-   if ((complex = lGetElemStr(complex_list, CX_name, "host"))) {
-      fillComplexFromHost(new_complex_list, complex_list, host, complex, 
-            DOMINANT_LAYER_HOST, recompute_debitation_dependent);
-   }
+   complex = lGetElemStr(complex_list, CX_name, "host");
+   fillComplexFromHost(new_complex_list, complex_list, host, complex, 
+         DOMINANT_LAYER_HOST, recompute_debitation_dependent);
 
    DEXIT;
    return 0;
@@ -484,8 +483,8 @@ int recompute_debitation_dependent
    }
 
    /* build queue complex and add it to result */
-   if ((complex = lGetElemStr(complex_list, CX_name, "queue")))
-      fillComplexFromQueue(new_complex_list, complex_list, complex, queue, recompute_debitation_dependent);
+   complex = lGetElemStr(complex_list, CX_name, "queue");
+   fillComplexFromQueue(new_complex_list, complex_list, complex, queue, recompute_debitation_dependent);
 
    DEXIT;
    return 0;
@@ -527,7 +526,8 @@ int fillComplexFromHost(lList **new_complex, lList *complex_list,
    DENTER(TOP_LAYER, "fillComplexFromHost");
 
    /* append main complex "host"/"global" ... */
-   append_complexes(new_complex, complex, layer, recompute_debitation_dependent);
+   if (complex)
+      append_complexes(new_complex, complex, layer, recompute_debitation_dependent);
 
    if (!host) { /* there may be a queue which has no host object yet */
       DEXIT;
@@ -760,8 +760,8 @@ int recompute_debitation_dependent; /* recompute only attribute types which  */
    DENTER(TOP_LAYER, "fillComplexFromQueue");
 
    /* append main "queue" complex ... */
-   append_complexes(new_complex, complex, DOMINANT_LAYER_QUEUE, recompute_debitation_dependent);
-
+   if (complex)
+      append_complexes(new_complex, complex, DOMINANT_LAYER_QUEUE, recompute_debitation_dependent);
 
    if (!queue) {
       DEXIT;
@@ -1292,6 +1292,7 @@ char *object_name
       if (ret) {
          /* error message gets written by sge_fill_requests into SGE_EVENT */
          answer_list_add(alpp, SGE_EVENT, STATUS_EUNKNOWN, ANSWER_QUALITY_ERROR);
+         lFreeElem(tmp_elem);
          DEXIT;
          return STATUS_EUNKNOWN;
       }

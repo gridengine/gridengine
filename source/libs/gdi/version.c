344ac9a2e2d1a51981bf63d6047c194e3d4ccf0c
andy <andy>
2003-06-17 09:06:31 +0000

    version setting

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "5.3p3";
+const char GDI_VERSION[] = "5.3prep4";
 
 #ifdef ADD_COPYRIGHT
 #  include "copyright.h"

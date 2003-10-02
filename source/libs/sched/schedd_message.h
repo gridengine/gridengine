7fb7386ff8770070f7d468e6609d861efa7986f9
adoerr <adoerr>
2003-10-02 16:54:52 +0000

    AD-2003-10-02-1: Bugfix.:   Fixed the build                  Changed:   schedd_message.h

--- source/libs/sched/schedd_message.h
+++ source/libs/sched/schedd_message.h
@@ -56,7 +56,7 @@ void schedd_mes_commit(lList *job_list, int ignore_category);
 
 void schedd_mes_rollback(void);
 
-lList *schedd_mes_get_tmp_list();
+lList *schedd_mes_get_tmp_list(void);
 
 void schedd_mes_set_tmp_list(lListElem *category, int name, u_long32 job_number);
 

07b840a36b0c41e4d4457cc548aebf9fda893815
adoerr <adoerr>
2003-10-02 16:37:42 +0000

    AD-2003-10-02-0: Enhancem.: Added new library ('liblck') which provides                             a locking API to be used by lock clients and                             lock service providers.                  Review:    AH                  Changed:   new

---
+++ source/libs/lck/sge_lock.h
@@ -0,0 +1,119 @@
+#ifndef _SGE_LOCK_H_
+#define _SGE_LOCK_H_
+
+/*___INFO__MARK_BEGIN__*/
+/*************************************************************************
+ * 
+ *  The Contents of this file are made available subject to the terms of
+ *  the Sun Industry Standards Source License Version 1.2
+ * 
+ *  Sun Microsystems Inc., March, 2001
+ * 
+ * 
+ *  Sun Industry Standards Source License Version 1.2
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.2 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+ * 
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ * 
+ *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ * 
+ *   Copyright: 2003 by Sun Microsystems, Inc.
+ * 
+ *   All Rights Reserved.
+ * 
+ ************************************************************************/
+/*___INFO__MARK_END__*/
+
+#include "basis_types.h"
+#include "sgermon.h"
+
+
+typedef enum {
+   LOCK_GLOBAL = 0,                      /* global lock */
+   LOCK_EVENT_CLIENT_LST = 1,            /* event client list lock */
+   LOCK_JOB_REPORT_LST = 2,              /* job report list lock */
+   LOCK_EXECD_CONFIG_LST = 3,            /* execd configuration list lock */
+   LOCK_SCHEDD_CONFIG_LST = 4,           /* schedd configuration list lock */
+   LOCK_MASTER_CONFIG_LST = 5,           /* master configuration list lock */
+   LOCK_MASTER_JOB_LST = 6,              /* master job list lock */
+   LOCK_MASTER_ZOMBIE_LST = 7,           /* master zombie list lock */
+   LOCK_MASTER_CALENDAR_LST = 8,         /* master calendar list lock */
+   LOCK_MASTER_COMPLEX_ENTRY_LST = 9,    /* master complex entry list lock */
+   LOCK_MASTER_CKPT_OBJ_LST = 10,        /* master checkpoint object list lock */
+   LOCK_MASTER_CLUSTER_QUEUE_LST = 11   ,/* master cluster queue list lock */
+   LOCK_MASTER_QUEUE_LST = 12,           /* master queue list lock */
+   LOCK_MASTER_USER_MAPPING_LST = 13,    /* master user mapping list lock */
+   LOCK_MASTER_HOST_GROUP_LST = 14,      /* master host group list lock */
+   LOCK_MASTER_EXEC_HOST_LST = 15,       /* master exec host list lock */
+   LOCK_MASTER_ADMIN_HOST_LST = 16,      /* master admin host list lock */
+   LOCK_MASTER_SUBMIT_HOST_LST = 17,     /* master submit host list lock */
+   LOCK_MASTER_JOB_SCHEDD_INFO_LST = 18, /* master job schedd info list lock */
+   LOCK_MASTER_MANAGER_LST = 19,         /* master manager list lock */
+   LOCK_MASTER_OPERATOR_LST = 20,        /* master operator list lock */
+   LOCK_MASTER_PARALLEL_ENV_LST = 21,    /* master parallel environment list lock */
+   LOCK_MASTER_SHARETREE_LST = 22,       /* master sharetree list lock */
+   LOCK_MASTER_USER_LST = 23,            /* master user list lock */
+   LOCK_MASTER_SUBMIT_USER_LST = 24,     /* master submit user list lock */
+   LOCK_MASTER_USER_SET_LST = 25,        /* master user set list lock */
+   LOCK_MASTER_PROJECT_LST = 26,         /* master project list lock */
+   NUM_OF_TYPES = 27
+} sge_locktype_t;
+
+typedef enum {
+   LOCK_READ  = 1, /* shared  */
+   LOCK_WRITE = 2  /* exclusive */
+} sge_lockmode_t;
+
+typedef u_long32 sge_locker_t;
+
+/*
+ * Lock user interface
+ */
+void sge_lock(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID);
+void sge_unlock(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID);
+sge_locker_t sge_locker_id(void);
+
+const char* sge_type_name(sge_locktype_t aType);
+int sge_num_locktypes(void);
+
+#if defined(SGE_LOCK)
+#error "SGE_LOCK already defined!"
+#endif
+
+#define SGE_LOCK(type, mode) \
+{ \
+   const char *name = sge_type_name(type); \
+   DPRINTF(("%s() line %d: about to lock \"%s\"\n", __FILE__, __LINE__, name)); \
+   sge_lock(type, mode, sge_locker_id()); \
+   DPRINTF(("%s() line %d: locked \"%s\"\n", __FILE__, __LINE__, name)); \
+}
+
+#if defined(SGE_UNLOCK)
+#error "SGE_UNLOCK already defined!"
+#endif
+
+#define SGE_UNLOCK(type, mode) \
+{ \
+   const char *name = sge_type_name(type); \
+   DPRINTF(("%s() line %d: about to unlock \"%s\"\n", __FILE__, __LINE__, name)); \
+   sge_unlock(type, mode, sge_locker_id()); \
+   DPRINTF(("%s() line %d: unlocked \"%s\"\n", __FILE__, __LINE__, name)); \
+}
+
+/*
+ * Lock service provider interface 
+ */
+void sge_set_lock_callback(void (*aFunc)(sge_locktype_t, sge_lockmode_t, sge_locker_t)); 
+void sge_set_unlock_callback(void (*aFunc)(sge_locktype_t, sge_lockmode_t, sge_locker_t));
+void sge_set_id_callback(sge_locker_t (*aFunc)(void));
+
+#endif /* _SGE_LOCK_H_ */
---
+++ source/libs/lck/test_sge_lock_main.c
@@ -0,0 +1,275 @@
+/*___INFO__MARK_BEGIN__*/
+/*************************************************************************
+ * 
+ *  The Contents of this file are made available subject to the terms of
+ *  the Sun Industry Standards Source License Version 1.2
+ * 
+ *  Sun Microsystems Inc., March, 2001
+ * 
+ * 
+ *  Sun Industry Standards Source License Version 1.2
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.2 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+ * 
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ * 
+ *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ * 
+ *   Copyright: 2003 by Sun Microsystems, Inc.
+ * 
+ *   All Rights Reserved.
+ * 
+ ************************************************************************/
+/*___INFO__MARK_END__*/
+
+#include "test_sge_lock_main.h"
+
+#include <pthread.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "sge_lock.h"
+
+
+static pthread_mutex_t *locks;
+
+static void setup(void);
+static void teardown(void);
+static sge_locker_t id_callback(void);
+static void lock_callback(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID);
+static void unlock_callback(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID);
+
+
+/****** test_sge_lock_main/main() **********************************************
+*  NAME
+*     main() -- Generic skeleton for lock API test programs. 
+*
+*  SYNOPSIS
+*     int main(int argc, char *argv[]) 
+*
+*  FUNCTION
+*     Generic driver for lock API test programs utilizing pthreads and pthread
+*     mutexes. Setup locking API by registering the needed callbacks. Determine
+*     the number of threads needed. Create threads and execute the provided thread
+*     function. Wait until all the threads have finished. Teardown the locking API.
+*
+*     If a program wants to utilize this skeleton it has to implement the functions
+*     of a small API defined in 'sge_lock_main.h'.
+*
+*  INPUTS
+*     int argc     - number of arguments 
+*     char *argv[] - argument array 
+*
+*  RESULT
+*     int - always 0
+*
+*  SEE ALSO
+*     lck/test_sge_lock_main.h
+*******************************************************************************/
+int main(int argc, char *argv[])
+{
+   pthread_t *t;
+   int i,j;
+
+   setup();
+
+   i = get_thrd_demand();
+   t = (pthread_t *)malloc(i * sizeof(pthread_t));
+
+   printf("Create %d threads\n", i);
+   for(j = 0; j < i; j++) {
+      pthread_create(&(t[j]), NULL, get_thrd_func(), get_thrd_func_arg());
+   }
+
+   for(j = 0; j < i; j++) {
+      printf("Join thread %u\n", (unsigned int)t[j]);
+      pthread_join(t[j], NULL);
+   }
+
+   teardown();
+   free(t);
+
+   return 0;
+} /* main */
+
+/****** test_sge_lock_main/setup() *********************************************
+*  NAME
+*     setup() -- Setup the locking API 
+*
+*  SYNOPSIS
+*     static void setup(void) 
+*
+*  FUNCTION
+*     Create and initialize the pthread mutexes needed. Register the callbacks
+*     required by the locking API. 
+*
+*  INPUTS
+*     void - none 
+*
+*  RESULT
+*     static void - none 
+*
+*  SEE ALSO
+*     test_sge_lock_main/teardown()
+*******************************************************************************/
+static void setup(void)
+{
+   int n, i;
+
+   printf("Setup\n");
+
+   n = sge_num_locktypes();
+   locks = (pthread_mutex_t *)malloc(n * sizeof(pthread_mutex_t));
+
+   for(i = 0; i < n; i++) {
+      pthread_mutex_init(&(locks[i]), NULL);
+   }
+
+   sge_set_lock_callback(lock_callback);
+   sge_set_unlock_callback(unlock_callback);
+   sge_set_id_callback(id_callback);
+
+   return;
+} /* setup */
+
+/****** test_sge_lock_main/teardown() ******************************************
+*  NAME
+*     teardown() -- Teardown the locking API 
+*
+*  SYNOPSIS
+*     static void teardown(void) 
+*
+*  FUNCTION
+*     Destroy and free the pthread mutexes created during the setup. 
+*
+*  INPUTS
+*     void - none 
+*
+*  RESULT
+*     static void - none 
+*
+*  EXAMPLE
+*     ??? 
+*
+*  NOTES
+*     ??? 
+*
+*  BUGS
+*     ??? 
+*
+*  SEE ALSO
+*     test_sge_lock_main/setup()
+*******************************************************************************/
+static void teardown(void)
+{
+   int n, i;
+
+   printf("Teardown\n");
+
+   n = sge_num_locktypes();
+
+   for (i = 0; i < n; i++) {
+      pthread_mutex_destroy(&(locks[i]));
+   }
+   
+   free(locks);
+
+   return;
+} /* teardown */
+
+/****** test_sge_lock_main/id_callback() ***************************************
+*  NAME
+*     id_callback() -- Locker ID callback implementation
+*
+*  SYNOPSIS
+*     static sge_locker_t id_callback(void) 
+*
+*  FUNCTION
+*     Return if of current thread as locker id. 
+*
+*  INPUTS
+*     void - none 
+*
+*  RESULT
+*     static sge_locker_t - locker id 
+*
+*  SEE ALSO
+*     test_sge_lock_main/setup()
+*     lck/sge_lock.c
+*******************************************************************************/
+static sge_locker_t id_callback(void)
+{
+   return (sge_locker_t)pthread_self();
+} /* id_callback */
+
+/****** test_sge_lock_main/lock_callback() *************************************
+*  NAME
+*     lock_callback() -- Lock callback implementation 
+*
+*  SYNOPSIS
+*     static void lock_callback(sge_locktype_t aType, sge_lockmode_t aMode, 
+*     sge_locker_t anID) 
+*
+*  FUNCTION
+*     Lock pthread mutex determined by 'aType' in mode 'aMode'.
+*
+*  INPUTS
+*     sge_locktype_t aType - lock type 
+*     sge_lockmode_t aMode - lock mode 
+*     sge_locker_t anID    - locker id 
+*
+*  RESULT
+*     static void - none
+*
+*  SEE ALSO
+*     test_sge_lock_main/setup()
+*     lck/sge_lock.c
+*******************************************************************************/
+static void lock_callback(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID)
+{
+   printf("Locker %d tries to lock %s\n", (int)anID, sge_type_name(aType));
+   pthread_mutex_lock(&(locks[aType]));
+   printf("Locker %d locked %s\n", (int)anID, sge_type_name(aType));
+
+   return;
+} /* lock_callback */
+
+/****** test_sge_lock_main/unlock_callback() ***********************************
+*  NAME
+*     unlock_callback() -- Unlock callback implementation 
+*
+*  SYNOPSIS
+*     static void unlock_callback(sge_locktype_t aType, sge_lockmode_t aMode, 
+*     sge_locker_t anID) 
+*
+*  FUNCTION
+*     Unlock pthread mutex determined by 'aType'. 
+*
+*  INPUTS
+*     sge_locktype_t aType - lock type 
+*     sge_lockmode_t aMode - lock mode 
+*     sge_locker_t anID    - locker id 
+*
+*  RESULT
+*     static void - none
+*
+*  SEE ALSO
+*     test_sge_lock_main/setup()
+*     lck/sge_lock.c
+*******************************************************************************/
+static void unlock_callback(sge_locktype_t aType, sge_lockmode_t aMode, sge_locker_t anID)
+{
+   pthread_mutex_unlock(&(locks[aType]));
+   printf("Locker %d unlocked %s\n", (int)anID, sge_type_name(aType));
+
+   return;
+} /* lock_callback */
+

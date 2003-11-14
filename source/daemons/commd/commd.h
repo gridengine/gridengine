2463f65cd2d253d249c2c0abb9c8dcdbd0e84a4d
adoerr <adoerr>
2003-11-14 14:49:34 +0000

    AD-2003-11-14-0: Bugfix:    Value of 'MAXID' was to large to be used in                             conjunction with 'u_short'.                  Changed:   commd

--- source/daemons/commd/commd.h
+++ source/daemons/commd/commd.h
@@ -38,8 +38,8 @@ extern "C" {
 #define MAXHOSTLEN 256
 #define MAXCOMPONENTLEN 256
 
-#define MAXID 0xffff
-#define MINID 1
+#define MAXID 0xff  /* max commproc ID */
+#define MINID 1     /* min commproc ID */
 #define STRLEN_ID 4
 
 

e7fa639930137bf1934c2e2a1ac035b7aad1cf99
Joachim Gabler <jgabler@univa.com>
2004-02-18 21:29:16 +0000

    JG-2004-02-18-1: Bugfix:    Fixed build for dbstat. Issue number: Obtained from: Submitted by: Reviewed by:

--- source/utilbin/msg_utilbin.h
+++ source/utilbin/msg_utilbin.h
@@ -157,4 +157,15 @@
 #define MSG_SPOOLINIT_COMMANDINTRO7   _MESSAGE(57067, _("vacuum            compress database, update statistics\n"))
 #define MSG_SPOOLINIT_COMMANDINTRO8   _MESSAGE(57068, _("info              output information about the database\n"))
 #define MSG_SPOOLINIT_COMMANDINTRO9   _MESSAGE(57069, _("method            output the compiled in spooling method\n"))
+
+/*
+ * utilbin/dbstat
+ */
+#define MSG_DBSTAT_COMMANDINTRO1   _MESSAGE(57070,         _("database query and maintenance\n"))
+#define MSG_DBSTAT_COMMANDINTRO2   _MESSAGE(57071,         _("following are the valid commands:\n"))
+#define MSG_DBSTAT_LIST            _MESSAGE(57072,         _("list [object type]  list all objects [matching object type]\n"))
+#define MSG_DBSTAT_DUMP            _MESSAGE(57073,         _("dump key            dump the objects matching key\n"))
+#define MSG_DBSTAT_DELETE          _MESSAGE(57074,         _("delete key          delete the objects matching key\n"))
+
+
 #endif /* __MSG_UTILBIN_H */

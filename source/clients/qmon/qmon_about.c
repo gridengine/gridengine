109dc5921c75299deaad86a97fc9312a66ed8eaa
andy <andy>
2004-03-09 09:53:38 +0000

    AS-2004-03-09-0 productization

--- source/clients/qmon/qmon_about.c
+++ source/clients/qmon/qmon_about.c
@@ -47,7 +47,7 @@ static char header[] = "@fBWelcome %s@@%s,@fR\n\nYou are using @fB%s@fR in cell 
 extern char SFLN_ELN[];
 
 #ifdef ADD_SUN_COPYRIGHT
-static char mailto[] = "\nFor further information and feedback use: @fBsgebeta53-support@@.sun.com@fR\n\n";
+static char mailto[] = "\nFor further information and feedback use: @fBn1ge6beta-support@@.sun.com@fR\n\n";
 #else
 static char mailto[] = "\n";
 #endif

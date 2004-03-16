825e3792922ac6a1733e96234e0800fad9776c83
Ernst Bablick <ebablick@univa.com>
2004-03-16 13:27:01 +0000

    EB-2004-03-16-0: Bugfix:    Hostgroup identifiers not verified in queue_conf(5)                  Issue:     856                  Changed:   sge_qmaster                  Review:    SG

--- source/libs/sgeobj/msg_sgeobjlib.h
+++ source/libs/sgeobj/msg_sgeobjlib.h
@@ -276,16 +276,12 @@
 #define MSG_QSTAT_WRONGGCHAR_C         _MESSAGE(64276, _("wrong character \'%c\' for -g option\n"))
 #define MSG_CQUEUE_UNKNOWNCENTRY_S     _MESSAGE(64277, _("Complex attribute "SFQ" does not exist\n"))
 #define MSG_OBJECT_ALREADYEXIN_SSS     _MESSAGE(64278, _("No modification because "SFQ" already exists in "SFQ" of "SFQ"\n"))
-
-/*
-** sge_conf.c
-*/
 #define MSG_CONF_USING_SS              _MESSAGE(64300, _("using "SFQ" for "SFN"\n"))
 #define MSG_CONF_NOCONFIGFROMMASTER    _MESSAGE(64301, _("could not get configuration from qmaster - using defaults\n"))
 #define MSG_CONF_NEITHERSGECODGRDSETTINGSGE _MESSAGE(64302, _("neither SGE nor COD nor GRD environment for jobs configured, setting SGE environment\n"))
 #define MSG_CENTRY_QINOTALLOWED        _MESSAGE(64303, _("Queue instance names are not allowed in -l resource requests\n"))
 #define MSG_OBJECT_VALUENOTULONG_S     _MESSAGE(64304, _(SFQ" is not an u_long32 value\n"))
-#define MSG_HGRP_INVALIDHOSTGROUPNAME_S     _MESSAGE(64305, _("host group name "SFQ" is not valid\n"))
-
+#define MSG_CQUEUE_INVALIDDOMSETTING_S _MESSAGE(64305, _("No modification for host group "SFQ" which does not exist.\n"))
+#define MSG_HGRP_INVALIDHOSTGROUPNAME_S     _MESSAGE(64306, _("host group name "SFQ" is not valid\n"))
 
 #endif /* __MSG_SGEOBJLIB_H */

d0a6a0acd0f11e23d331ef5e2ea6d78476b63771
crei <crei>
2004-03-18 14:43:41 +0000

    CR-2004-03-18-0: Cleanup:   added parameter check in cl_commlib_receive_message()                  Changed:   commlib Issue number: Obtained from: Submitted by: Reviewed by:

--- source/libs/comm/cl_commlib.c
+++ source/libs/comm/cl_commlib.c
@@ -2667,6 +2667,10 @@ int cl_commlib_receive_message(cl_com_handle_t* handle,char* un_resolved_hostnam
       return CL_RETVAL_PARAMS;
    }
 
+   if (handle == NULL) {
+      return CL_RETVAL_PARAMS;
+   }
+
    if (synchron != 0) {
       gettimeofday(&now,NULL);
       my_timeout = now.tv_sec + handle->synchron_receive_timeout;
@@ -2676,11 +2680,6 @@ int cl_commlib_receive_message(cl_com_handle_t* handle,char* un_resolved_hostnam
       CL_LOG(CL_LOG_INFO,"message filtering not supported");
    }
    do {
-      /* If the handle was shutdown, give up. */
-      if (handle == NULL) {
-         break;
-      }
-      
       cl_raw_list_lock(handle->connection_list);
       elem = cl_connection_list_get_first_elem(handle->connection_list);     
       if (elem == NULL) {

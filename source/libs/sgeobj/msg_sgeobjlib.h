b7ed7ac8bc1c7743926f63390ac542901b909f03
Ernst Bablick <ebablick@univa.com>
2004-03-25 14:06:12 +0000

    EB-2004-03-25-0: Bugfix:    qconf  -mq rejects pe_list config that is                             accepted with -aq                             (Fix of EB-2004-03-16-0)                  Changed:   qconf, sge_qmaster                  Issue:     906, 856                  Review:    SG

--- source/libs/sgeobj/msg_sgeobjlib.h
+++ source/libs/sgeobj/msg_sgeobjlib.h
@@ -280,7 +280,7 @@
 #define MSG_CONF_NOCONFIGFROMMASTER    _MESSAGE(64301, _("could not get configuration from qmaster - using defaults\n"))
 #define MSG_CENTRY_QINOTALLOWED        _MESSAGE(64303, _("Queue instance names are not allowed in -l resource requests\n"))
 #define MSG_OBJECT_VALUENOTULONG_S     _MESSAGE(64304, _(SFQ" is not an u_long32 value\n"))
-#define MSG_CQUEUE_INVALIDDOMSETTING_S _MESSAGE(64305, _("No modification for host group "SFQ" which does not exist.\n"))
+#define MSG_CQUEUE_INVALIDDOMSETTING_SS _MESSAGE(64305, _("Did not modify "SFQ" for host group "SFQ" which does not exist.\n"))
 #define MSG_HGRP_INVALIDHOSTGROUPNAME_S     _MESSAGE(64306, _("host group name "SFQ" is not valid\n"))
 
 #endif /* __MSG_SGEOBJLIB_H */

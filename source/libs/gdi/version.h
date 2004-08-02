832b7b4f80fbd38a01806830021f74caa70a7204
andy <andy>
2004-08-02 07:27:05 +0000

    .

--- source/libs/gdi/version.h
+++ source/libs/gdi/version.h
@@ -32,7 +32,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-#define GDI_VERSION "6.0"
+#define GDI_VERSION "6.0u1"
 
 #ifdef ADD_SUN_COPYRIGHT
 #  define GE_LONGNAME "N1 Grid Engine"

938c934b955831360d18b8c784e929baf648e573
Ernst Bablick <ebablick@univa.com>
2004-10-13 13:13:20 +0000

    EB-2004-10-13-0: Enhancem.: qstat requests only necessary jobs (qstat -s -u)                  Changed:   qstat, qhost, libuti, sge_qstat(5), qstat(1)                  Review:    SG

---
+++ source/clients/qstat/qstat_cmdline.h
@@ -0,0 +1,53 @@
+#ifndef __MSG_QSTAT_COMMANDLINE_H
+#define __MSG_QSTAT_COMMANDLINE_H
+/*___INFO__MARK_BEGIN__*/
+/*************************************************************************
+ * 
+ *  The Contents of this file are made available subject to the terms of
+ *  the Sun Industry Standards Source License Version 1.2
+ * 
+ *  Sun Microsystems Inc., March, 2001
+ * 
+ * 
+ *  Sun Industry Standards Source License Version 1.2
+ *  =================================================
+ *  The contents of this file are subject to the Sun Industry Standards
+ *  Source License Version 1.2 (the "License"); You may not use this file
+ *  except in compliance with the License. You may obtain a copy of the
+ *  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+ * 
+ *  Software provided under this License is provided on an "AS IS" basis,
+ *  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+ *  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+ *  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+ *  See the License for the specific provisions governing your rights and
+ *  obligations concerning the Software.
+ * 
+ *   The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+ * 
+ *   Copyright: 2001 by Sun Microsystems, Inc.
+ * 
+ *   All Rights Reserved.
+ * 
+ ************************************************************************/
+/*___INFO__MARK_END__*/
+
+const char *
+get_root_qstat_file_path(dstring *file_path);
+
+const char *
+get_home_qstat_file_path(dstring *file_path, lList **answer_list);
+
+int 
+qstat_usage(int qselect_mode, FILE *fp, char *what);
+
+bool
+switch_list_qstat_parse_from_file(lList **switch_list, lList **answer_list,
+                                  int mode, const char *file);
+
+bool
+switch_list_qstat_parse_from_cmdline(lList **ppcmdline, lList **answer_list,
+                                     int qselect_mode, char **argv);
+
+
+#endif /* __MSG_QSTAT_COMMANDLINE_H */

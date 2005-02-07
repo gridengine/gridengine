bc20dbb8be8626fbc16dfa58dc51689f02609f82
andy <andy>
2005-02-07 16:05:47 +0000

    set version string

--- source/libs/gdi/version.h
+++ source/libs/gdi/version.h
@@ -32,7 +32,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-#define GDI_VERSION "6.0u3"
+#define GDI_VERSION "6.0u4beta"
 
 #ifdef ADD_SUN_COPYRIGHT
 #  define GE_LONGNAME "N1 Grid Engine"

c140c16fefa81f03e20ab42c1585c4b1a79c35b9
Ernst Bablick <ebablick@univa.com>
2005-04-05 11:51:00 +0000

    EB-2005-04-05-0: Enhancem.: added loadsensor script for windows systems                  Review:    HP

--- source/dist/util/install_modules/inst_common.sh
+++ source/dist/util/install_modules/inst_common.sh
@@ -808,6 +808,10 @@ PrintLocalConf()
    if [ $RSH_DAEMON != "undef" ]; then
       $ECHO "rsh_daemon             $RSH_DAEMON"
    fi
+   if [ $LOADSENSOR_COMMAND != "undef" ]; then
+      $ECHO "load_sensor            $LOADSENSOR_COMMAND"
+   fi
+
 }
 
 

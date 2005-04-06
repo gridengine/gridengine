850fb9b2c8649d24f75d07d34d7582be521460c7
crei <crei>
2005-04-06 14:48:27 +0000

    CR-2005-04-06-7: Testsuite: added missing file for qsub tests (-sync)
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

---
+++ testsuite/scripts/qsub_exit.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+#
+#___INFO__MARK_BEGIN__
+##########################################################################
+#
+#  The Contents of this file are made available subject to the terms of
+#  the Sun Industry Standards Source License Version 1.2
+#
+#  Sun Microsystems Inc., March, 2001
+#
+#
+#  Sun Industry Standards Source License Version 1.2
+#  =================================================
+#  The contents of this file are subject to the Sun Industry Standards
+#  Source License Version 1.2 (the "License"); You may not use this file
+#  except in compliance with the License. You may obtain a copy of the
+#  License at http://gridengine.sunsource.net/Gridengine_SISSL_license.html
+#
+#  Software provided under this License is provided on an "AS IS" basis,
+#  WITHOUT WARRANTY OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING,
+#  WITHOUT LIMITATION, WARRANTIES THAT THE SOFTWARE IS FREE OF DEFECTS,
+#  MERCHANTABLE, FIT FOR A PARTICULAR PURPOSE, OR NON-INFRINGING.
+#  See the License for the specific provisions governing your rights and
+#  obligations concerning the Software.
+#
+#  The Initial Developer of the Original Code is: Sun Microsystems, Inc.
+#
+#  Copyright: 2001 by Sun Microsystems, Inc.
+#
+#  All Rights Reserved.
+#
+##########################################################################
+#___INFO__MARK_END__
+
+#
+# Usage: exit.sh [time]]
+#        default for time is 60 seconds
+
+# -- our name ---
+#$ -N Exit
+#$ -S /bin/sh
+
+/bin/echo Here I am. Exiting now at: `date`
+
+arg=1
+if [ $# -ge 1 ]; then
+   arg=$1
+fi
+exit $arg

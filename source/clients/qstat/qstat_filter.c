91b62fe794fe718842ea2aa3550c87d3284eb09f
sgrell <sgrell>
2005-05-25 13:56:57 +0000

    SG-2005-05-25-0: Bugfix:    fix qstat -t again (SG-2005-01-21-0 was not enough)                  Review:    EB                  Changed:   qstat                  Issue:     1420                  Bugtraq:   6218877

--- source/clients/qstat/qstat_filter.c
+++ source/clients/qstat/qstat_filter.c
@@ -300,11 +300,13 @@ void qstat_filter_add_t_attributes(void)
    
    const int nm_JAT_Type_list[] = {
       JAT_task_list,
+      JAT_usage_list,
       NoName
    };
 
    const int nm_JAT_Type_template[] = {
       JAT_task_list,
+      JAT_usage_list,
       NoName
    };
    

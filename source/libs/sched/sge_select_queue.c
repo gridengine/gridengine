0ba34f6b2faaaba9ef2c41ac360764b89c424db4
sgrell <sgrell>
2005-06-02 10:16:07 +0000

    SG-2005-06-02-0: Bugfix:    qconf -tsm command never stops writing output                  Changed:   scheduler                  Bugtraq:   6279409                  Issue:     1616                  Review:    JG

--- source/libs/sched/sge_select_queue.c
+++ source/libs/sched/sge_select_queue.c
@@ -2291,16 +2291,18 @@ u_long32 ttype
 *     lList **free - Input queue instance list and return free slots.
 *     lList **full - If non-NULL the full queue instances get returned here.
 *
+*
+*  TODO: take a look into list hashing, not needed for temp list
+*
 *  RESULT
 *     int - 0 success 
 *          -1 error
 *******************************************************************************/
 int sge_split_queue_slots_free(lList **free, lList **full) 
 {
-   lList *lp = NULL;
-   int do_free_list = 0;
-   lListElem *this, *next;
-   bool full_queues = false;
+   lList *full_queues = NULL;
+   lListElem *this = NULL;
+   lListElem *next = NULL;
 
    DENTER(TOP_LAYER, "sge_split_queue_nslots_free");
 
@@ -2309,26 +2311,24 @@ int sge_split_queue_slots_free(lList **free, lList **full)
       return -1;
    }
 
-   if (full == NULL) {
-       full = &lp;
-       do_free_list = 1;
-   }
-
    for (this=lFirst(*free); ((next=lNext(this))), this ; this = next) {
-      if ((qinstance_slots_used(this)+1) > (int) lGetUlong(this, QU_job_slots)) {
-
+      if (qinstance_slots_used(this) >= (int) lGetUlong(this, QU_job_slots)) {
+          
+         this = lDechainElem(*free, this);        
+          
          if (!qinstance_state_is_full(this)) {
             schedd_mes_add_global(SCHEDD_INFO_QUEUEFULL_, lGetString(this, QU_full_name));
-            full_queues = true;
+            qinstance_state_set_full(this, true);
+
+            if (full_queues == NULL) { 
+               full_queues = lCreateListHash("full one", lGetListDescr(*free), false);
+            }
+            lAppendElem(full_queues, this);             
          }   
-      
-         /* chain 'this' into 'full' list */
-         this = lDechainElem(*free, this);
-         if (full) {
-            if (!*full) {
+         else if (full != NULL) {
+            if (*full == NULL) {
                *full = lCreateList("full one", lGetListDescr(*free));
             }   
-            qinstance_state_set_full(this, true);
             lAppendElem(*full, this);
          } 
          else {
@@ -2337,21 +2337,28 @@ int sge_split_queue_slots_free(lList **free, lList **full)
       }
    }
 
-   if (*full) {
-
-      if (full_queues) { 
-         schedd_log_list(MSG_SCHEDD_LOGLIST_QUEUESFULLANDDROPPED , *full, QU_full_name);
-      }   
-
-      if (do_free_list) {
-         lFreeList(*full);
-         *full = NULL;
+   /* dump out the -tsm log and add the new queues to the disabled queue list */
+   if (full_queues) {
+      schedd_log_list(MSG_SCHEDD_LOGLIST_QUEUESFULLANDDROPPED , full_queues, QU_full_name);
+      if (full != NULL) {
+         if (*full == NULL) {
+            *full = full_queues;
+            full_queues = NULL;
+         }
+         else {
+            lAddList(*full, full_queues);
+            full_queues = NULL;
+         }
+      }
+      else {
+         full_queues = lFreeList(full_queues);
       }
    }
 
    DEXIT;
    return 0;
 }
+
 
 /* ----------------------------------------
 

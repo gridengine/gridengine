fbdea9ecd3e04bd3a2942e9be5fca5bc24320b0a
roland <roland>
2005-06-24 11:34:08 +0000

    RD-2005-06-24-1: Bugfix:   fixed broken build on linux

--- source/daemons/common/qlogin_starter.h
+++ source/daemons/common/qlogin_starter.h
@@ -31,7 +31,7 @@
  * 
  ************************************************************************/
 /*___INFO__MARK_END__*/
-int delete_qrsh_pid_file();
+int delete_qrsh_pid_file(void);
 int qlogin_starter(const char *cwd, char *daemon, char **env);
 int write_to_qrsh(const char *data);
 void write_exit_code_to_qrsh(int exit_code);

cc8a8a233acb8674785c4bfc6e965096a747f6df
roland <roland>
2005-09-13 14:39:40 +0000

    RD-2005-09-13-1: Bugfix:    qmaster keeps sending signals to execds                  Changed:   qmaster                  Issue:     1665                  Bugtraq:   6286510                  Review:    AH

--- source/daemons/qmaster/sge_c_ack.c
+++ source/daemons/qmaster/sge_c_ack.c
@@ -193,11 +193,12 @@ static void sge_c_job_ack(char *host, char *commproc, u_long32 ack_tag,
          }
       }
       
-      DPRINTF(("QUEUE %s: SIGNAL ACK\n", lGetString(qinstance, QU_qname)));
-               lSetUlong(qinstance, QU_pending_signal, 0);
-      te_delete_one_time_event(TYPE_SIGNAL_RESEND_EVENT, 0, 0, lGetString(qinstance, QU_qname));
+      DPRINTF(("QUEUE %s: SIGNAL ACK\n", lGetString(qinstance, QU_full_name)));
+
+      lSetUlong(qinstance, QU_pending_signal, 0);
+      te_delete_one_time_event(TYPE_SIGNAL_RESEND_EVENT, 0, 0, lGetString(qinstance, QU_full_name));
       spool_write_object(&answer_list, spool_get_default_context(), qinstance, 
-                         lGetString(qinstance, QU_qname), SGE_TYPE_QINSTANCE);
+                         lGetString(qinstance, QU_full_name), SGE_TYPE_QINSTANCE);
       answer_list_output(&answer_list);
       break;
 

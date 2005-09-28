840e062b86421dc6317d2e91a58b5f3ea981c55d
crei <crei>
2005-09-28 16:00:40 +0000

    CR-2005-09-28-0: Bugfix:    qping core dump with enabled message content dump                             (merged)                  Issue:     1810                  Bugtraq:   6327427                  Review:    AA
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- source/libs/comm/cl_xml_parsing.h
+++ source/libs/comm/cl_xml_parsing.h
@@ -445,6 +445,7 @@ int cl_xml_parse_SIRM(unsigned char* buffer, unsigned long buffer_length, cl_com
 int cl_xml_parse_CCM(unsigned char* buffer, unsigned long buffer_length, cl_com_CCM_t** message );
 int cl_xml_parse_CCRM(unsigned char* buffer, unsigned long buffer_length, cl_com_CCRM_t** message );
 
-
+int cl_com_transformString2XML(const char* input, char** output);
+int cl_com_transformXML2String(const char* input, char** output);
 #endif /* __CL_XML_PARSING_H */
 

fcba5bd181bfb357c649d7c028c2dc8a8b4f749f
crei <crei>
2005-12-09 08:44:01 +0000

    CR-2005-12-09-0: Testsuite: Using longer timeouts for spool directory                             removal                  Review:    Pending
    Issue number:
    Obtained from:
    Submitted by:
    Reviewed by:

--- testsuite/tcl_files/file_procedures.tcl
+++ testsuite/tcl_files/file_procedures.tcl
@@ -2225,18 +2225,18 @@ proc remote_delete_directory { hostname path } {
       puts $CHECK_OUTPUT "delete_directory - moving \"$path\" to trash folder ..."
       set new_name [ file tail $path ] 
 
-      start_remote_prog $hostname "ts_def_con2" "mv" "$path $CHECK_TESTSUITE_ROOT/testsuite_trash/$new_name.[timestamp]" prg_exit_state 60 0 "" 1 0 1
+      start_remote_prog $hostname "ts_def_con2" "mv" "$path $CHECK_TESTSUITE_ROOT/testsuite_trash/$new_name.[timestamp]" prg_exit_state 300 0 "" 1 0 1
       if { $prg_exit_state != 0 } {
          puts $CHECK_OUTPUT "delete_directory - mv error"
          puts $CHECK_OUTPUT "delete_directory - try to copy the directory"
-         start_remote_prog $hostname "ts_def_con2" "cp" "-r $path $CHECK_TESTSUITE_ROOT/testsuite_trash/$new_name.[timestamp]" prg_exit_state 60 0 "" 1 0 1
+         start_remote_prog $hostname "ts_def_con2" "cp" "-r $path $CHECK_TESTSUITE_ROOT/testsuite_trash/$new_name.[timestamp]" prg_exit_state 300 0 "" 1 0 1
          if { $prg_exit_state != 0 } {
             puts $CHECK_OUTPUT "could not mv/cp directory \"$path\" to trash folder"
             add_proc_error "remote_delete_directory" -1 "$hostname: could not mv/cp directory \"$path\" to trash folder"
             set return_value -1
          } else { 
             puts $CHECK_OUTPUT "copy ok -  removing directory"
-            start_remote_prog $hostname "ts_def_con2" "rm" "-rf $path" prg_exit_state 60 0 "" 1 0 1
+            start_remote_prog $hostname "ts_def_con2" "rm" "-rf $path" prg_exit_state 300 0 "" 1 0 1
             if { $prg_exit_state != 0 } {
                puts $CHECK_OUTPUT "could not remove directory \"$path\""
                add_proc_error "remote_delete_directory" -1 "$hostname: could not remove directory \"$path\""

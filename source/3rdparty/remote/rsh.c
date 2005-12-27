4b3c165f88190958169034e7bddc1f4b42c4df4a
Joachim Gabler <jgabler@univa.com>
2005-12-27 14:58:40 +0000

    JG-2005-12-27-0: Bugfix:    Merged JG-2005-12-23-0 from maintrunk:                             - fixed a bug in rsh client                  Bugtraq:   6366691                  Review:    RD Issue number: Obtained from: Submitted by: Reviewed by:

--- source/3rdparty/remote/rsh.c
+++ source/3rdparty/remote/rsh.c
@@ -141,7 +141,7 @@ main(int argc, char *argv[])
 	struct passwd *pw;
 	struct servent *sp;
 	long omask = 0;
-	int argoff, asrsh, ch, dflag, nflag, one, rem;
+	int argoff, ch, dflag, nflag, one, rem;
 	pid_t pid = 0;
 	uid_t uid;
 	char *args, *host, *p, *user;
@@ -156,7 +156,7 @@ main(int argc, char *argv[])
   sigset_t set;
 #endif
 
-	argoff = asrsh = dflag = nflag = 0;
+	argoff = dflag = nflag = 0;
 	one = 1;
 	host = user = NULL;
 
@@ -167,8 +167,6 @@ main(int argc, char *argv[])
 		p = argv[0];
 	if (strcmp(p, "rsh"))
 		host = p;
-	else
-		asrsh = 1;
 
 	/* handle "rsh host flags" */
 	if (!host && argc > 2 && argv[1][0] != '-') {
@@ -241,13 +239,9 @@ main(int argc, char *argv[])
 	if (!host && !(host = argv[optind++]))
 		usage();
 
-	/* if no further arguments, must have been called as rlogin. */
-	if (!argv[optind]) {
-		if (asrsh)
-			*argv = "rlogin";
-		execvp("rlogin", argv);
-		fprintf(stderr, "can't exec %s", "rlogin"); 
-	}
+	/* if no further arguments, error. We don't call rlogin! */
+	if (!argv[optind])
+      usage();
 
 	argc -= optind;
 	argv += optind;

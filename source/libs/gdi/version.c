b5ee81e243c7f40591a1c0b232aea642cdd2f101
roland <roland>
2006-04-18 13:15:50 +0000

    RD-2006-04-18-0: CVS        Created V60u8_TAG ---------------------------------------------

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -30,7 +30,7 @@
  ************************************************************************/
 /*___INFO__MARK_END__*/
 
-const char GDI_VERSION[] = "6.0u8rc1";
+const char GDI_VERSION[] = "6.0u8";
 
 #ifdef ADD_SUN_COPYRIGHT
 const char GE_LONGNAME[] = "N1 Grid Engine";

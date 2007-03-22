622d4b160c0faf33932c7639b54c301af7a2cf11
roland <roland>
2007-03-22 14:30:15 +0000

    RD-2007-03-22-0: Bugfix:    Corrected and validated qquota schema file                  Review:    RH

--- source/clients/qquota/qquota.c
+++ source/clients/qquota/qquota.c
@@ -98,7 +98,7 @@ static int destroy_xml_report_handler(report_handler_t** handler, lList **alpp);
 
 static int xml_report_started(report_handler_t* handler, lList **alpp) {
    printf("<?xml version='1.0'?>\n");
-   printf("<qquota_result xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\">\n");
+   printf("<qquota_result xmlns=\"http://gridengine.sunsource.net/61/qquota\">\n");
    return QQUOTA_SUCCESS;
 }
 

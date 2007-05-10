cc7a1b827ada8f1319cff82d20c5a81899124ab6
mpospisil <mpospisil>
2007-05-10 09:46:54 +0000

    MP-2007-05-10-2  Bugfix:    Queue request -q from sge_request can't                             be overridden through command line, fixed code.                  Bugster:   6328064                  Review:    JG

--- source/clients/common/read_defaults.c
+++ source/clients/common/read_defaults.c
@@ -649,7 +649,8 @@ void opt_list_merge_command_lines(lList **opts_all,
       if (*opts_all == NULL) {
          *opts_all = *opts_scriptfile;
       } else {
-          lAddList(*opts_all, opts_cmdline);
+         /* Override the values from defaults */
+         lOverrideStrList(*opts_all, *opts_scriptfile, SPA_switch);
       }
       *opts_scriptfile = NULL;
    }
@@ -657,7 +658,8 @@ void opt_list_merge_command_lines(lList **opts_all,
       if (*opts_all == NULL) {
          *opts_all = *opts_cmdline;
       } else {
-          lAddList(*opts_all, opts_cmdline);
+         /* Override values from both defaults and scriptfile */
+         lOverrideStrList(*opts_all, *opts_cmdline, SPA_switch);
       }
       *opts_cmdline = NULL;
    }

c3eb3a9d175c4f385482e5fa890892e691fb3429
andreas <andreas>
2007-05-14 16:05:18 +0000

    AH-2007-05-14-0: Bugfix:    Memory leak in libdrmaa.so                  Bugster:   6551121                  Review:    LP Issue number: Obtained from: Submitted by: Reviewed by:

--- source/libs/gdi/sge_gdi_ctx.c
+++ source/libs/gdi/sge_gdi_ctx.c
@@ -204,14 +204,6 @@ u_long32 gdi_state_get_next_request_id(void)
    return gdi_state->request_id;
 }
 
-#ifdef USE_GDI_STATE
-static void gdi_state_set_made_setup(int i)
-{
-   GET_SPECIFIC(gdi_state_t, gdi_state, gdi_state_init, gdi_state_key, "gdi_state_set_made_setup");
-   gdi_state->made_setup = i;
-}
-#endif
-
 
 int gdi_state_get_made_setup(void)
 {
@@ -692,11 +684,12 @@ static void sge_gdi_ctx_class_error(sge_gdi_ctx_class_t *thiz, int error_type, i
 
 static void sge_gdi_ctx_set_is_setup(sge_gdi_ctx_class_t *thiz, bool is_setup)
 {
-   sge_gdi_ctx_t *gdi_ctx = NULL;
+   sge_gdi_ctx_t *gdi_ctx;
 
    DENTER(TOP_LAYER, "sge_gdi_ctx_set_is_setup");
 
    if (!thiz || !thiz->sge_gdi_ctx_handle) {
+      DPRINTF(("CTX: couldn't set %s\n", is_setup?"true":"false"));
       DRETURN_VOID;
    }   
    
@@ -710,16 +703,18 @@ static void sge_gdi_ctx_set_is_setup(sge_gdi_ctx_class_t *thiz, bool is_setup)
 
 static bool sge_gdi_ctx_is_setup(sge_gdi_ctx_class_t *thiz)
 {
-   sge_gdi_ctx_t *gdi_ctx = NULL;
+   sge_gdi_ctx_t *gdi_ctx;
 
    DENTER(TOP_LAYER, "sge_gdi_ctx_is_setup");
 
    if (!thiz || !thiz->sge_gdi_ctx_handle) {
+      DPRINTF(("CTX: couldn't return true/false\n"));
       DRETURN(false);
    }   
    
    gdi_ctx = (sge_gdi_ctx_t*)thiz->sge_gdi_ctx_handle;
-      
+   DPRINTF(("CTX: return %s\n", gdi_ctx->is_setup?"true":"false"));
+
    DRETURN(gdi_ctx->is_setup);
 }
 
@@ -2065,57 +2060,32 @@ int sge_setup2(sge_gdi_ctx_class_t **context, u_long32 progid, lList **alpp)
 */
 int sge_gdi2_setup(sge_gdi_ctx_class_t **context_ref, u_long32 progid, lList **alpp)
 {
-   sge_gdi_ctx_class_t *ctx = NULL;
    int ret = AE_OK;
    bool alpp_was_null = true;
 
    DENTER(TOP_LAYER, "sge_gdi2_setup");
 
-#ifdef USE_GDI_STATE 
-   if (alpp != NULL && *alpp != NULL) {
-     alpp_was_null = false;
+   if (sge_gdi_ctx_is_setup(*context_ref)) {
+      if (alpp_was_null) {
+         SGE_ADD_MSG_ID(sprintf(SGE_EVENT, MSG_GDI_GDI_ALREADY_SETUP));
+      } else {
+         answer_list_add_sprintf(alpp, STATUS_EEXIST, ANSWER_QUALITY_WARNING,
+                                 MSG_GDI_GDI_ALREADY_SETUP);
+      }
+      DRETURN(AE_ALREADY_SETUP);
    }
 
-   /* TODO: gdi_state_t should be part of context */
-   /* initialize libraries */
-   pthread_once(&gdi_once_control, gdi_once_init);
-   if (gdi_state_get_made_setup()) {
-      if (alpp_was_null) {
-         SGE_ADD_MSG_ID(sprintf(SGE_EVENT, MSG_GDI_GDI_ALREADY_SETUP));
-      } else {
-         answer_list_add_sprintf(alpp, STATUS_EEXIST, ANSWER_QUALITY_WARNING,
-                                 MSG_GDI_GDI_ALREADY_SETUP);
-      }
-      DRETURN(AE_ALREADY_SETUP);
-   }
-#else   
-   if (sge_gdi_ctx_is_setup(ctx)) {
-      if (alpp_was_null) {
-         SGE_ADD_MSG_ID(sprintf(SGE_EVENT, MSG_GDI_GDI_ALREADY_SETUP));
-      } else {
-         answer_list_add_sprintf(alpp, STATUS_EEXIST, ANSWER_QUALITY_WARNING,
-                                 MSG_GDI_GDI_ALREADY_SETUP);
-      }
-      DRETURN(AE_ALREADY_SETUP);
-   }
-#endif
    ret = sge_setup2(context_ref, progid, alpp); 
    if (ret != AE_OK) {
       DRETURN(ret);
    }
 
-   ctx = *context_ref;
-   if (ctx->prepare_enroll(ctx) != CL_RETVAL_OK) {
-      sge_gdi_ctx_class_get_errors(ctx, alpp, true);
+   if ((*context_ref)->prepare_enroll(*context_ref) != CL_RETVAL_OK) {
+      sge_gdi_ctx_class_get_errors(*context_ref, alpp, true);
       DRETURN(AE_QMASTER_DOWN);
    }
 
-#ifdef USE_GDI_STATE
-   /* TODO: gdi_state should be part of context */
-   gdi_state_set_made_setup(1);
-#else   
-   sge_gdi_ctx_set_is_setup(ctx, true);
-#endif
+   sge_gdi_ctx_set_is_setup(*context_ref, true);
 
    DRETURN(AE_OK);
 }

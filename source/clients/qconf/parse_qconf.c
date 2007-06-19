6868a40662c0e253b81fc71ced416f907acb58a3
ravinallan <ravinallan>
2007-06-19 10:26:18 +0000

    Bugfix : Wrong exit code with qconf -sds. Bugster: 6522273 Obtained from: Submitted by: Reviewed by: JG

--- source/clients/qconf/parse_qconf.c
+++ source/clients/qconf/parse_qconf.c
@@ -5171,7 +5171,7 @@ char *argv[]
       if (strcmp("-sds", *spp) == 0) {
          lList *answer_list = NULL;
 
-         cqueue_list_sick(ctx, &answer_list);
+         sge_parse_return = cqueue_list_sick(ctx, &answer_list)?0:1;
          show_gdi_request_answer(answer_list);
          lFreeList(&answer_list);
          spp++;

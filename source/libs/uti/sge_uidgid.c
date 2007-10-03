e2f620dcfb530e1e990f388dbb8b02e486b336fb
ravinallan <ravinallan>
2007-10-03 10:14:47 +0000

    RN-2007-10-03-0: Bugfix: SGE does not work if primary group entry is too big                          in groups map Bugster        : 6568575 Issue number   : 2249 Reviewed by    : AH

--- source/libs/uti/sge_uidgid.c
+++ source/libs/uti/sge_uidgid.c
@@ -849,6 +849,8 @@ int get_pw_buffer_size(void)
 #ifdef _SC_GETPW_R_SIZE_MAX
    if ((sz = (int)sysconf(_SC_GETPW_R_SIZE_MAX)) == -1) {
       sz = buf_size;
+   } else {
+      sz = MAX(sz, buf_size);
    }
 #endif
 
@@ -885,6 +887,8 @@ int get_group_buffer_size(void)
 #ifdef _SC_GETGR_R_SIZE_MAX
    if ((sz = (int)sysconf(_SC_GETGR_R_SIZE_MAX)) == -1) {
       sz = buf_size;
+   } else {
+      sz = MAX(sz, buf_size);
    }
 #endif
 

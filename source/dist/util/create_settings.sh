8f500a7580eaabf2d675977c9bead271459faccd
Harald Pollinger <hpollinger@univa.com>
2007-12-21 22:26:56 +0000

    HP-2007-12-21-1: Bugfix:    Fixed wrong library path in settings.[c]sh file on HP1164                  Changed:   dist/util/create_settings.sh                  Review:    MD

--- source/dist/util/create_settings.sh
+++ source/dist/util/create_settings.sh
@@ -102,6 +102,7 @@ echo "set path = ( \$SGE_ROOT/bin/"'$ARCH $path )'               >> $SP_CSH
 echo 'switch ($ARCH)'                                            >> $SP_CSH
 #ENFORCE_SHLIBPATH#echo 'case "sol*":'                           >> $SP_CSH
 #ENFORCE_SHLIBPATH#echo 'case "lx*":'                            >> $SP_CSH
+#ENFORCE_SHLIBPATH#echo 'case "hp11-64":'                        >> $SP_CSH
 #ENFORCE_SHLIBPATH#echo '   breaksw'                             >> $SP_CSH
 echo 'case "*":'                                                 >> $SP_CSH
 echo "   set shlib_path_name = \`\$SGE_ROOT/util/arch -lib\`"       >> $SP_CSH
@@ -154,7 +155,7 @@ echo "PATH=\$SGE_ROOT/bin/\$ARCH:\$PATH; export PATH"            >> $SP_SH
 
 echo '# library path setting required only for architectures where RUNPATH is not supported' >> $SP_SH
 echo 'case $ARCH in'                                                >> $SP_SH
-#ENFORCE_SHLIBPATH#echo 'sol*|lx*)'                                 >> $SP_SH
+#ENFORCE_SHLIBPATH#echo 'sol*|lx*|hp11-64)'                         >> $SP_SH
 #ENFORCE_SHLIBPATH#echo '   ;;'                                     >> $SP_SH
 echo '*)'                                                           >> $SP_SH
 echo "   shlib_path_name=\`\$SGE_ROOT/util/arch -lib\`"             >> $SP_SH

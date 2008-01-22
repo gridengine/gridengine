5a90d6e8a61ddcd3ba882ac07447ea7d77af0951
dom <dom>
2008-01-22 13:35:11 +0000

    MD-2008-01-22-1: Bugfix:    changed output string, which brakes testuite test                  Changed:   install

--- source/dist/util/install_modules/inst_common.sh
+++ source/dist/util/install_modules/inst_common.sh
@@ -584,9 +584,6 @@ CheckConfigFile()
    MIN_GID=100        #from 0 - 100 may be reserved GIDs
    is_valid="true"
 
-   #translate the CELL_NAME entry to SGE_CELL as needed by installscript   
-   SGE_CELL=$CELL_NAME
-
    CONFIG_ENTRIES=`grep -v "^\#" $CONFIG_FILE | cut -d"=" -f1`
 
    if [ "$BACKUP" = "true" ]; then
@@ -606,10 +603,10 @@ CheckConfigFile()
          is_valid="false" 
       fi
       if [ -z "$SGE_CELL" ]; then
-         $INFOTEXT -e "Your >CELL_NAME< entry is not set!" 
+         $INFOTEXT -e "Your >SGE_CELL< entry is not set!" 
          is_valid="false"
       elif [ ! -d "$SGE_ROOT/$SGE_CELL" ]; then
-         $INFOTEXT -e "Your >CELL_NAME< directory %s does not exist!" $SGE_ROOT/$SGE_CELL
+         $INFOTEXT -e "Your >SGE_CELL< directory %s does not exist!" $SGE_ROOT/$SGE_CELL
          is_valid="false"
       fi
       if [ -z "$BACKUP_DIR" ]; then
@@ -641,6 +638,9 @@ CheckConfigFile()
       fi
       return
    fi
+
+   #translate the CELL_NAME entry to SGE_CELL as needed by installscript   
+   SGE_CELL=$CELL_NAME
 
    #do hostname resolving. fetching hostname from config file, try to resolve and
    #and recreate the hostname lists
@@ -800,7 +800,7 @@ CheckConfigFile()
          $INFOTEXT -e "Your >SGE_JMX_PORT< entry is invalid. It must be a number between 1 and 65536!"
          is_valid="false"
       fi
-      if [ -z "$SGE_JVM_LIB_PATH" -o  "`echo "$SGE_JVM_LIB_PATH" | cut -d"/" -f1`" = "`echo "$SGE_JVM_LIB_PATH" | cut -d"/" -f2`" ]; then
+      if [ "$SGE_ENABLE_JMX" = "true" -a \( -z "$SGE_JVM_LIB_PATH" -o  "`echo "$SGE_JVM_LIB_PATH" | cut -d"/" -f1`" = "`echo "$SGE_JVM_LIB_PATH" | cut -d"/" -f2`" \) ]; then
          $INFOTEXT -e "Your >SGE_JVM_LIB_PATH< is empty or an invalid path. It must be a full path!"
          is_valid="false"
       fi   

68c3bc3475000f13d19ea142aeff7c6100afee45
mpospisil <mpospisil>
2008-03-04 15:17:46 +0000

    MP-2008-03-04-0: Bugfix:    Fixed error introduced into build through                             checkin of MP-2008-02-22-0                  Review:    MD

--- source/clients/qstat/qstat.c
+++ source/clients/qstat/qstat.c
@@ -1954,12 +1954,12 @@ u_long32 isXML
          }
       }
    }
-   what = lWhat("%T(->%T%I%I%I%I%I%I%I%I%I%I%I%I%I%I%I->%T%I%I%I%I%I%I->%T%I%I%I%I->%T(%I%I%I%I%I)"
-           "%I%I%I%I->%T(%I)%I->%T(%I)%I%I%I%I%I%I%I%I%I%I%I%I%I%I%I->%T%I%I%I%I%I%I%I)",
-            JB_Type, RN_Type, JB_ja_structure, JB_job_number, JB_exec_file, JB_submission_time, JB_owner,
+   what = lWhat("%T(%I%I%I%I%I%I%I%I%I%I%I%I%I%I->%T%I%I%I%I%I%I->%T%I%I%I%I->%T(%I%I%I%I%I)"
+            "%I%I%I%I->%T(%I)%I->%T(%I)%I%I%I%I%I%I%I%I%I%I%I%I%I%I%I->%T%I%I%I%I%I%I%I)%T(%I)",
+            JB_Type, JB_job_number, JB_exec_file, JB_submission_time, JB_owner,
             JB_uid, JB_group, JB_gid, JB_account, JB_merge_stderr, JB_mail_list,
             JB_project, JB_notify, JB_job_name, JB_stdout_path_list, PN_Type,
-            JB_jobshare, JB_hard_resource_list, JB_soft_resource_list,
+  	    JB_jobshare, JB_hard_resource_list, JB_soft_resource_list,
             JB_hard_queue_list, JB_soft_queue_list, JB_shell_list, PN_Type,
             JB_env_list, JB_job_args, JB_script_file, JB_ja_tasks,
             JAT_Type, JAT_status, JAT_task_number, JAT_scaled_usage_list,
@@ -1970,8 +1970,8 @@ u_long32 isXML
             JB_mail_options, JB_stdin_path_list, JB_priority, JB_restart, JB_verify,
             JB_master_hard_queue_list, JB_script_size, JB_pe, RN_Type, JB_pe_range,
             JB_jid_request_list, JB_verify_suitable_queues, JB_soft_wallclock_gmt,
-            JB_hard_wallclock_gmt, JB_override_tickets,
-            JB_version); 
+            JB_hard_wallclock_gmt, JB_override_tickets, JB_version,
+            RN_Type, JB_ja_structure); 
    /* get job list */
    alp = ctx->gdi(ctx, SGE_JOB_LIST, SGE_GDI_GET, &jlp, where, what);
    lFreeWhere(&where);

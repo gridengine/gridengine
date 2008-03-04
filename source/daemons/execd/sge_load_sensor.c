e6b40c4c4c928f190f55d8c30c13f38dad315ca7
roland <roland>
2008-03-04 13:40:40 +0000

    RD-2008-03-04-1: Bugfix:    loadsensor does not work or AIX51                  Issue:     2508                  Bugster:   6661060                  Review:    AH

--- source/daemons/execd/sge_load_sensor.c
+++ source/daemons/execd/sge_load_sensor.c
@@ -62,6 +62,8 @@ static void sge_set_ls_fds(lListElem *this_ls, fd_set *fds);
 static lListElem *sge_ls_create_ls(const char* qualified_hostname, char *name, const char *scriptfile);
 static void sge_ls_start_ls(const char *qualified_hostname, lListElem *this_ls);
 static void sge_ls_stop_ls(lListElem *this_ls, int send_no_quit_command);
+static int sge_ls_start(const char* qualified_hostname, const char *binary_path, char *scriptfile);
+
 static int read_ls(void);
 
 /* 
@@ -674,7 +676,7 @@ void sge_ls_gnu_ls(int gnu_ls)
 *  RESULT
 *     LS_OK
 ******************************************************************************/
-int sge_ls_start(const char *qualified_hostname, const char *binary_path, char *scriptfiles)
+static int sge_ls_start(const char *qualified_hostname, const char *binary_path, char *scriptfiles)
 {
    lListElem *ls_elem = NULL;        /* LS_Type */
    lListElem *nxt_ls_elem = NULL;    /* LS_Type */
@@ -734,6 +736,11 @@ int sge_ls_start(const char *qualified_hostname, const char *binary_path, char *
       ls_elem = lGetElemStr(ls_list, LS_command, scriptfiles_buffer);
       if (ls_elem == NULL) {
          ls_elem = sge_ls_create_ls(qualified_hostname, IDLE_LOADSENSOR_NAME, scriptfiles_buffer);
+
+         if (ls_list == NULL) {
+            ls_list = lCreateList("", LS_Type);
+         }
+         lAppendElem(ls_list, ls_elem);
       }
       if (ls_elem != NULL) {
          lSetUlong(ls_elem, LS_tag, 0);
@@ -754,6 +761,11 @@ int sge_ls_start(const char *qualified_hostname, const char *binary_path, char *
       ls_elem = lGetElemStr(ls_list, LS_command, scriptfiles_buffer);
       if (ls_elem == NULL) {
          ls_elem = sge_ls_create_ls(qualified_hostname, GNU_LOADSENSOR_NAME, scriptfiles_buffer);
+
+         if (ls_list == NULL) {
+            ls_list = lCreateList("", LS_Type);
+         }
+         lAppendElem(ls_list, ls_elem);
       }
       if (ls_elem != NULL) {
          lSetUlong(ls_elem, LS_tag, 0);

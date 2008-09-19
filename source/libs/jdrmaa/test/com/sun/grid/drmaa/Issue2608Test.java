d54d37255e26a919366c0f655e941e46706739c5
aja <aja>
2008-09-19 11:09:13 +0000

    AP-2008-09-19-0: Task:        drmaa junit test Issue2608test.java - output redirection                  Issue:       testsuite issue 180                  Review:      CR

--- source/libs/jdrmaa/test/com/sun/grid/drmaa/Issue2608Test.java
+++ source/libs/jdrmaa/test/com/sun/grid/drmaa/Issue2608Test.java
@@ -43,35 +43,35 @@ import junit.framework.*;
 import org.ggf.drmaa.*;
 
 /**
- * When a job is in hold due to -hold_jid dependency this is not indicated 
- * as STATE_SYSTEM_ON_HOLD (=17) by drmaa_job_ps(3) as it must be. 
+ * When a job is in hold due to -hold_jid dependency this is not indicated
+ * as STATE_SYSTEM_ON_HOLD (=17) by drmaa_job_ps(3) as it must be.
  * Instead STATE_QUEUED_ACTIVE (=16) is returned for such jobs.
  */
 public class Issue2608Test extends TestCase {
     private static final String SLEEPER;
     private Session session;
-    
+
     static {
         SLEEPER = Settings.get(Settings.SCRIPTS_DIR) + "/sleeper.sh";
     }
-    
+
     public static Test suite () {
         TestSuite suite = new TestSuite (Issue2608Test.class);
         return suite;
     }
 
     /**
-     * Setup of test. 
-     * @throws DrmaaException 
+     * Setup of test.
+     * @throws DrmaaException
      */
     public void setUp() throws DrmaaException {
         session = SessionFactory.getFactory().getSession();
         session.init("");
     }
-    
+
     /**
      * Tear down of test.
-     * @throws DrmaaException 
+     * @throws DrmaaException
      */
     public void tearDown() throws DrmaaException {
          try {
@@ -80,21 +80,21 @@ public class Issue2608Test extends TestCase {
             ex.printStackTrace();
         }
     }
-    
+
     /**
-     * Test of issue 2608: 
-     * - job hold due to -hold_jid is not indicated as 
+     * Test of issue 2608:
+     * - job hold due to -hold_jid is not indicated as
      *   STATE_SYSTEM_ON_HOLD by drmaa_job_ps(3)
-     * 
+     *
      * @throws org.ggf.drmaa.DrmaaException
      */
     public void test2608Test() throws DrmaaException {
         System.out.println("testIssue2608");
 
-        /** create and submit 30 sec. sleeper job */ 
+        /** create and submit 30 sec. sleeper job */
         JobTemplate jt = this.createSleeperTemplate(20);
         String job_id_1 = session.runJob(jt);
-            
+
         /** let the job wait till the other wait job finished */
         jt.setNativeSpecification(" -hold_jid " + job_id_1);
 
@@ -102,7 +102,7 @@ public class Issue2608Test extends TestCase {
         String job_id_2 = session.runJob(jt);
 
         /** make a short nip (15 sec) until job is scheduled */
-        try {    
+        try {
             Thread.sleep(15000);
         } catch (Exception e) {
             e.printStackTrace();
@@ -110,14 +110,14 @@ public class Issue2608Test extends TestCase {
         /** second job must be in suspended state and NOT in QUEUED ACTIVE */
         int status = session.getJobProgramStatus(job_id_2);
         assertTrue(status != Session.QUEUED_ACTIVE);
-        
+
         /** wait until first job has finished */
         final int timeout = 20;
         int time = 0;
-        
+
         while(true) {
-             if (session.getJobProgramStatus(job_id_1) == Session.DONE 
-              || session.getJobProgramStatus(job_id_1) == Session.FAILED 
+             if (session.getJobProgramStatus(job_id_1) == Session.DONE
+              || session.getJobProgramStatus(job_id_1) == Session.FAILED
               || time > timeout) {
                  break;
              }
@@ -126,22 +126,22 @@ public class Issue2608Test extends TestCase {
                 /** make a short nip (5 sec) */
                 Thread.sleep(1000);
             } catch (Exception e) {
-                fail("Failed while sleeping!" 
+                fail("Failed while sleeping!"
                          + e.getMessage());
             }
             time++;
         }
         time = 0;
         /** wait until second job has finished */
-        while(!(session.getJobProgramStatus(job_id_2) == Session.DONE 
+        while(!(session.getJobProgramStatus(job_id_2) == Session.DONE
               || session.getJobProgramStatus(job_id_2) == Session.FAILED
               || (time < timeout))) {
-            
+
             try {
                 /** make a short nip (1 sec) */
                 Thread.sleep(1000);
             } catch (Exception e) {
-                fail("Failed while sleeping!" 
+                fail("Failed while sleeping!"
                          + e.getMessage());
             }
             time++;
@@ -150,20 +150,22 @@ public class Issue2608Test extends TestCase {
 
     /**
      * Generates a JobTemplate for a sleeper job.
-     * 
+     *
      * @param sleep Time in second to sleep.
-     * @return JobTemplate 
+     * @return JobTemplate
      * @throws org.ggf.drmaa.DrmaaException
      */
-    private JobTemplate createSleeperTemplate(final int sleep) 
+    private JobTemplate createSleeperTemplate(final int sleep)
             throws DrmaaException {
-        
+
         JobTemplate jt = session.createJobTemplate();
-        
+
         jt.setRemoteCommand(SLEEPER);
         jt.setArgs(Collections.singletonList(Integer.toString(sleep)));
-        
+        jt.setOutputPath(":/dev/null");
+        jt.setErrorPath(":/dev/null");
+
         return jt;
     }
-    
+
 }

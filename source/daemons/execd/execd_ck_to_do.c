d801e85289105ab34c3d0350eabf63c484638fd9
roland <roland>
2008-09-30 09:35:38 +0000

    RD-2008-09-30-0: Enhancem.:  - Improved scheduling time by using hash                  Bugfix      - runtime limits were delayed after execd restart                  Issue:      2743                  Bugster:    6754094                  Review:     CR

--- source/daemons/execd/execd_ck_to_do.c
+++ source/daemons/execd/execd_ck_to_do.c
@@ -435,6 +435,10 @@ int do_ck_to_do(sge_gdi_ctx_class_t *ctx) {
                   lSetUlong(jep, JB_hard_wallclock_gmt, 
                             MIN(lGetUlong(jep, JB_hard_wallclock_gmt), duration_add_offset(now, task_wallclock_limit)));
                }
+               if (!mconf_get_simulate_jobs()) {
+                  job_write_spool_file(jep, lGetUlong(jatep, JAT_task_number), 
+                                       NULL, SPOOL_WITHIN_EXECD);
+               }
             }
             
             if (now >= lGetUlong(jep, JB_hard_wallclock_gmt) ) {
@@ -480,7 +484,7 @@ int do_ck_to_do(sge_gdi_ctx_class_t *ctx) {
 
    if (next_old_job <= now) {
       next_old_job = now + OLD_JOB_INTERVAL;
-      clean_up_old_jobs(0);
+      clean_up_old_jobs(ctx, 0);
    }
 
    /* check for end of simulated jobs */

27e12fc9d9a2fff9865e6bb0193217232396ba3b
Joachim Gabler <jgabler@univa.com>
2008-11-18 08:16:04 +0000

    JG-2008-11-18-0: Bugfix:      - inst_sge -rst: can't resolve username "none"                               - inst_sge -rst: doesn't restore resource_quotas                                 and advance reservation with classic spooling                  Bugster:     6751309, 6751310                  Issue:       2791, 2789                  Review:      MD Issue number: Obtained from: Submitted by: Reviewed by:

--- source/dist/util/install_modules/inst_common.sh
+++ source/dist/util/install_modules/inst_common.sh
@@ -1984,7 +1984,7 @@ RestoreConfig()
    BUP_SPOOL_FILE_LIST="jobseqnum"
    BUP_CLASSIC_COMMON_FILE_LIST="configuration sched_configuration accounting bootstrap qtask settings.sh act_qmaster sgemaster host_aliases settings.csh sgeexecd shadow_masters"
    BUP_CLASSIC_DIR_LIST="sgeCA local_conf" 
-   BUP_CLASSIC_SPOOL_FILE_LIST="jobseqnum admin_hosts calendars centry ckpt cqueues exec_hosts hostgroups managers operators pe projects qinstances schedd submit_hosts usermapping users usersets zombies"
+   BUP_CLASSIC_SPOOL_FILE_LIST="jobseqnum admin_hosts calendars centry ckpt cqueues exec_hosts hostgroups managers operators pe projects qinstances resource_quotas schedd submit_hosts usermapping users usersets zombies"
 
    MKDIR="mkdir -p"
    CP="cp -f"
@@ -2793,6 +2793,9 @@ RestoreCheckBootStrapFile()
       db_home=`cat $BACKUP_DIR/bootstrap | grep "spooling_params" | awk '{ print $2 }'`
       master_spool=`cat $BACKUP_DIR/bootstrap | grep "qmaster_spool_dir" | awk '{ print $2 }'`
       ADMINUSER=`cat $BACKUP_DIR/bootstrap | grep "admin_user" | awk '{ print $2 }'`
+      if [ "$ADMINUSER" = "none" ]; then
+         ADMINUSER="default"
+      fi
 
       MASTER_PORT=`cat $BACKUP_DIR/sgemaster | grep "SGE_QMASTER_PORT=" | head -1 | awk '{ print $1 }' | cut -d"=" -f2 | cut -d";" -f1` 
 

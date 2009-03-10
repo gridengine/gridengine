f2363946ecf7ae2b103ef59243861be976196187
roland <roland>
2009-03-10 12:02:29 +0000

    RD-2009-03-10-0: Bugfix:      fixed qmaster abort at update from previous version                  Bugster:     6814823                  Issue:       2949                  Review:      JG

--- source/libs/spool/sge_spooling_utilities.c
+++ source/libs/spool/sge_spooling_utilities.c
@@ -430,11 +430,11 @@ bool spool_default_validate_func(lList **answer_list,
                   lSetBool(load_value, HL_static, true);
                }
 
-               /* necessary to setup actual list of exechost */
-               debit_host_consumable(NULL, object, master_centry_list, 0, true);
                /* necessary to init double values of consumable configuration */
                centry_list_fill_request(lGetList(object, EH_consumable_config_list), 
                      NULL, master_centry_list, true, false, true);
+               /* necessary to setup actual list of exechost */
+               debit_host_consumable(NULL, object, master_centry_list, 0, true);
 
                if (ensure_attrib_available(NULL, object, 
                                            EH_consumable_config_list)) {

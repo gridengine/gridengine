2c8267567ff37d306ca6055db5315e9d0f3afabb
dom <dom>
2009-11-09 13:27:53 +0000

    MD-2009-11-09-1: Bugfix:      o Fixed broken hadoop build.xml                  Enhancem:    o added version string

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -34,9 +34,9 @@
 
 #include "gdi/version.h"
 
-const char GDI_VERSION[] = "6.2u5alpha1";
+const char GDI_VERSION[] = "6.2u5alpha2";
 
-const gdi_ulong32 GRM_GDI_VERSION = 0x100020F4;
+const gdi_ulong32 GRM_GDI_VERSION = 0x100020F5;
 
 vdict_t GRM_GDI_VERSION_ARRAY[] = 
 {
@@ -60,6 +60,7 @@ vdict_t GRM_GDI_VERSION_ARRAY[] =
       { 0x100020F2, "6.2u3" },
       { 0x100020F3, "6.2u4" },
       { 0x100020F4, "6.2u5alpha1" },
+      { 0x100020F5, "6.2u5alpha2" },
       { 0, NULL}
 };
 

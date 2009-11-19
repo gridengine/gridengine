85c984c9ac9bd3f8aa48a54d63948b521f7b9137
dom <dom>
2009-11-19 13:41:40 +0000

    MD-2009-11-19-1: CVS:		Created V62u5beta2_TAG -------------------------------------------------------

--- source/libs/gdi/version.c
+++ source/libs/gdi/version.c
@@ -34,9 +34,9 @@
 
 #include "gdi/version.h"
 
-const char GDI_VERSION[] = "6.2u5beta1";
+const char GDI_VERSION[] = "6.2u5beta2";
 
-const gdi_ulong32 GRM_GDI_VERSION = 0x100020F6;
+const gdi_ulong32 GRM_GDI_VERSION = 0x100020F7;
 
 vdict_t GRM_GDI_VERSION_ARRAY[] = 
 {
@@ -62,6 +62,7 @@ vdict_t GRM_GDI_VERSION_ARRAY[] =
       { 0x100020F4, "6.2u5alpha1" },
       { 0x100020F5, "6.2u5alpha2" },
       { 0x100020F6, "6.2u5beta1" },
+      { 0x100020F7, "6.2u5beta2" },
       { 0, NULL}
 };
 

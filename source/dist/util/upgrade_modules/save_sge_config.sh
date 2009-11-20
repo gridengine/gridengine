897ed58b149cf5612901fa6b56b8815061bdb36c
Daniel Gruber <dgruber@univa.com>
2009-11-20 10:45:37 +0000

    DG-2009-11-20-1: Bugfix:     upgrade from 6.1 to 62u5 fails because of missing                              newly introduced complexes                  Bugster:    6902891                  Review:     CR

--- source/dist/util/upgrade_modules/save_sge_config.sh
+++ source/dist/util/upgrade_modules/save_sge_config.sh
@@ -284,6 +284,38 @@ if [ $? -ne 0 ]; then
 display_win_gui     dwg        BOOL        ==    YES         NO         0        0'  "$DEST_DIR/centry.bak" > "$DEST_DIR/centry" 
    rm -f "$DEST_DIR/centry.bak"
 fi
+#add m_core if missing (new in 62u5)
+cat  "$DEST_DIR/centry" | grep "^m_core " > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+   cp "$DEST_DIR/centry" "$DEST_DIR/centry.bak"
+   sed '3i\
+m_core              core       INT         <=    YES         NO         0        0'  "$DEST_DIR/centry.bak" > "$DEST_DIR/centry" 
+   rm -f "$DEST_DIR/centry.bak"
+fi
+#add m_socket if missing (new in 62u5)
+cat  "$DEST_DIR/centry" | grep "^m_socket " > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+   cp "$DEST_DIR/centry" "$DEST_DIR/centry.bak"
+   sed '3i\
+m_socket            socket     INT         <=    YES         NO         0        0'  "$DEST_DIR/centry.bak" > "$DEST_DIR/centry" 
+   rm -f "$DEST_DIR/centry.bak"
+fi
+#add m_topology if missing (new in 62u5)
+cat  "$DEST_DIR/centry" | grep "^m_topology " > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+   cp "$DEST_DIR/centry" "$DEST_DIR/centry.bak"
+   sed '3i\
+m_topology          topo       RESTRING    ==    YES         NO         NONE     0'  "$DEST_DIR/centry.bak" > "$DEST_DIR/centry" 
+   rm -f "$DEST_DIR/centry.bak"
+fi
+#add m_topology_inuse if missing (new in 62u5)
+cat  "$DEST_DIR/centry" | grep "^m_topology_inuse " > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+   cp "$DEST_DIR/centry" "$DEST_DIR/centry.bak"
+   sed '3i\
+m_topology_inuse    utopo      RESTRING    ==    YES         NO         NONE     0'  "$DEST_DIR/centry.bak" > "$DEST_DIR/centry" 
+   rm -f "$DEST_DIR/centry.bak"
+fi
 
 #     -sel                          <show execution hosts>
 list=`$QCONF -sel 2>/dev/null`

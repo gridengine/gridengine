5778d42164543f30c13b0e84e29242a6dddf66bc
Ernst Bablick <ebablick@univa.com>
2011-03-02 11:30:57 +0100

    fixed broken build and did cleanup in zerodepend

--- source/libs/ijs/sge_ijs_lib.c
+++ source/libs/ijs/sge_ijs_lib.c
@@ -1,7 +1,27 @@
 /* Portions of this code are Copyright 2011 Univa Inc. */
-#include <termio.h>
+#if defined(DARWIN) || defined(INTERIX)
+#  include <termios.h>
+#  include <sys/ioctl.h>
+#  include <grp.h>
+#elif defined(HP1164) || defined(HP11)
+#  include <termios.h>
+#  include <stropts.h>
+#elif defined(SOLARIS64) || defined(SOLARIS86) || defined(SOLARISAMD64)
+#  include <stropts.h>
+#  include <termio.h>
+#elif defined(IRIX65)
+#  include <sys/ioctl.h>
+#  include <stropts.h>
+#  include <termio.h>
+#elif defined(FREEBSD) || defined(NETBSD)
+#  include <termios.h>
+#else
+#  include <termio.h>
+#endif
+
+#include "uti/sge_rmon.h"
+
 #include "sge_ijs_comm.h"
-#include "sgermon.h"
 
 int continue_handler (COMM_HANDLE *comm_handle, char *hostname) {
   DENTER(TOP_LAYER, "ijs_suspend: continue_handler");
